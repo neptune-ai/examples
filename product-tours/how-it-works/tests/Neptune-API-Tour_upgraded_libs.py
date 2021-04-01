ce3aad51b0c0eb8f9df693ee6af05dd39724fcda
kamil-kaczmarek <kamil.kaczmarek@neptune.ml>
2021-04-01 13:33:13 +0200

    version bump to 0.9.0

--- product-tours/how-it-works/tests/Neptune-API-Tour_upgraded_libs.py
+++ product-tours/how-it-works/tests/Neptune-API-Tour_upgraded_libs.py
@@ -5,7 +5,7 @@
 
 # Setup
 
-get_ipython().system(' pip install neptune-client==0.5.5')
+get_ipython().system(' pip install neptune-client==0.9.0')
 
 get_ipython().system(' pip install --upgrade --quiet neptune-client')
 
