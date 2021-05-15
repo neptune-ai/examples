e6b59c59a4d4c4894b69781fcf7be7356431f24d
Siddhant Sadangi <siddhant.sadangi@gmail.com>
2021-05-15 20:11:18 +0530

    Added html support

--- integrations-and-supported-tools/matplotlib/scripts/Neptune_Matplotlib_Support.py
+++ integrations-and-supported-tools/matplotlib/scripts/Neptune_Matplotlib_Support.py
@@ -1,10 +1,13 @@
+# Import necessary libraries
 import matplotlib.pyplot as plt
 import neptune.new as neptune
 import numpy as np
 
+# Initialize Neptune and create a new run
 run = neptune.init(api_token='ANONYMOUS',
                    project='common/matplotlib-support')
 
+# Create a sample chart
 np.random.seed(42)
 data = np.random.randn(2, 100)
 figure, ax = plt.subplots(2, 2, figsize=(5, 5))
@@ -12,5 +15,10 @@ ax[0, 0].hist(data[0])
 ax[1, 0].scatter(data[0], data[1])
 ax[0, 1].plot(data[0], data[1])
 
+# Log static image to Neptune
 run['static-img'] = neptune.types.File.as_image(figure)
+
+# Log interactive image to Neptune
 run['interactive-img'] = neptune.types.File.as_html(figure)
+
+# Tracking will stop automatically once script execution is complete
