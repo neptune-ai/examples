bed1f891de12bcb91b4095bced5b4144b3219c98
Siddhant Sadangi <siddhant.sadangi@gmail.com>
2021-05-17 13:21:39 +0530

    Update Neptune_HTML_Support.py

--- integrations-and-supported-tools/html/scripts/Neptune_HTML_Support.py
+++ integrations-and-supported-tools/html/scripts/Neptune_HTML_Support.py
@@ -6,13 +6,9 @@ run = neptune.init(api_token='ANONYMOUS',
                    project='common/html-support')
 
 # Create a sample HTML object
-html = "<button type='button', style='background-color:#005879; width:400px; height:400px; font-size:30px'> \
-    <a style='color: #ccc', href='https://docs.neptune.ai'> Take me back to the docs!<a> </button>"
-
-with open('sample.html', 'w') as f:
-    f.write(html)
+html = "<button type='button', style='background-color:#005879; width:400px; height:400px; font-size:30px'> <a style='color: #ccc', href='https://docs.neptune.ai'> Take me back to the docs!<a> </button>"
 
 # Log HTML object
-run['html_obj'] = neptune.types.File('sample.html')
+run['html_obj'] = neptune.types.File.from_content(html, extension='html')
 
 # Tracking will stop automatically once script execution is complete
