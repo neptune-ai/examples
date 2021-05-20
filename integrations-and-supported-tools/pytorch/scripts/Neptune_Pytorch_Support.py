6014dc40c85459c835027d42bd78deae66d81883
Prince Canuma <prince.gdt@gmail.com>
2021-05-20 20:29:02 +0530

    :sparkles: PyTorch support examples

--- integrations-and-supported-tools/pytorch/scripts/Neptune_Pytorch_Support.py
+++ integrations-and-supported-tools/pytorch/scripts/Neptune_Pytorch_Support.py
@@ -5,156 +5,111 @@ import torch.optim as optim
 import torchvision
 from torchvision import datasets, models, transforms
 import neptune.new as neptune
-import hashlib
 import numpy as np
 
-import os
-os.environ['NEPTUNE_API_TOKEN']= 'ANONYMOUS'
 
-# Setting Up Neptune run 
+def main():
+    # Step 1: Initialize Neptune and create new run
+    run = neptune.init(project='common/pytorch-integration', api_token='ANONYMOUS', source_files=['*.py'])
 
-# Step 1: Initialize you project
-run = neptune.init(project = 'common/pytorch-integration')
+    # Experiment Config
+    data_dir = 'data/CIFAR10'
+    compressed_ds = './data/CIFAR10/cifar-10-python.tar.gz'
+    data_tfms = {
+            'train': transforms.Compose([
+                transforms.RandomHorizontalFlip(),
+                transforms.ToTensor(),
+                transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+            ])
+        }
 
-# Step 2: Create a namespace for all your experiment metadata
-base_namespace = 'experiment'
-ns_run = run[base_namespace]
-
-# Helper function
-
-def save_model(model, name ='model.txt'):
-    print(f'Saving model arch as {name}.txt')
-    with open(f'{name}_arch.txt', 'w') as f:  f.write(str(model))
-    print(f'Saving model weights as {name}.pth')
-    torch.save(model.state_dict(), f'./{name}.pth')
-
-# Experiment Config
-data_dir = 'data/CIFAR10'
-compressed_ds = './data/CIFAR10/cifar-10-python.tar.gz'
-sha = hashlib.sha1(compressed_ds.encode())
-data_tfms = {
-        'train': transforms.Compose([
-            transforms.RandomHorizontalFlip(),
-            transforms.ToTensor(),
-            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-        ]),
-        'val': transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-            
-        ])
+    params = {
+        'lr': 1e-2,
+        'bs': 128,
+        'input_sz': 32 * 32 * 3,
+        'n_classes': 10,
+        'epochs': 10,
+        'model_filename': 'basemodel',
     }
 
-parameters = {'lr': 1e-2,
-              'bs': 128,
-              'input_sz': 32 * 32 * 3,
-              'n_classes': 10,
-              'epochs': 2,
-              'model_filename': 'basemodel',
-              'device': torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-              }
+    # Model & Dataset
+    class BaseModel(nn.Module):
+        def __init__(self, input_sz, hidden_dim, n_classes):
+            super(BaseModel, self).__init__()
+            self.main = nn.Sequential(
+                nn.Linear(input_sz, hidden_dim*2),
+                nn.ReLU(),
+                nn.Linear(hidden_dim*2, hidden_dim),
+                nn.ReLU(),
+                nn.Linear(hidden_dim, hidden_dim//2),
+                nn.ReLU(),
+                nn.Linear(hidden_dim//2, n_classes)
+            )
 
-# Model & Dataset
-class BaseModel(nn.Module):
-    def __init__(self, input_sz, hidden_dim, n_classes):
-        super(BaseModel, self).__init__()
-        self.main = nn.Sequential(
-            nn.Linear(input_sz, hidden_dim*2),
-            nn.ReLU(),
-            nn.Linear(hidden_dim*2, hidden_dim),
-            nn.ReLU(),
-            nn.Linear(hidden_dim, hidden_dim//2),
-            nn.ReLU(),
-            nn.Linear(hidden_dim//2, n_classes)
-        )
+        def forward(self, input):
+            x = input.view(-1, 32* 32 * 3)
+            return self.main(x)
 
-    def forward(self, input):
-        x = input.view(-1, 32* 32 * 3)
-        return self.main(x)
+    trainset = datasets.CIFAR10(data_dir, transform=data_tfms['train'], download=True)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=params['bs'], shuffle=True, num_workers=2)
+    dataset_size = {'train': len(trainset)}
 
-trainset = datasets.CIFAR10(data_dir, transform=data_tfms['train'], 
-                            download=True)
-trainloader = torch.utils.data.DataLoader(trainset, 
-                                          batch_size=parameters['bs'],
-                                          shuffle=True, num_workers=2)
+    # Instatiate model, crit & opt
+    model = BaseModel(params['input_sz'], params['input_sz'], params['n_classes'])
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(model.parameters(), lr=params['lr'])
 
-      
-dataset_size = {'train': len(trainset), 'val': len(validset)}
+    # Step 2: Log config & pararameters
+    run['config/dataset/path'] = data_dir
+    run['config/dataset/transforms'] = data_tfms
+    run['config/dataset/size'] = dataset_size
+    run['config/model'] = type(model).__name__
+    run['config/criterion'] = type(criterion).__name__
+    run['config/optimizer'] = type(optimizer).__name__
+    run['config/params'] = params
 
-# iN
-model = BaseModel(parameters['input_sz'], parameters['input_sz'], parameters['n_classes']).to(parameters['device'])
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(model.parameters(), lr=parameters['lr'])
+    epoch_loss = 0.0
+    epoch_acc = 0.0
+    best_acc = 0.0
 
-# Training Loop
-
-# Step 3: Logging config & pararameters
-ns_run['io_files/resource'] = (compressed_ds, sha.hexdigest())
-ns_run['config/dataset/path'] = data_dir
-ns_run['config/dataset/transforms'] = data_tfms
-ns_run['config'] = parameters  
-ns_run['config/dataset/size'] = dataset_size
-ns_run['config/model'] = type(model).__name__
-ns_run['config/criterion'] = type(criterion).__name__
-ns_run['config/optimizer'] = type(optimizer).__name__
-
-epoch_loss = 0.0
-epoch_acc = 0.0
-best_acc = 0.0
-
-# Step 4: Log metrics and artifacts
-for epoch in range(parameters['epochs']):
-    running_loss = 0.0
-    running_corrects = 0
-
-    for i, (x, y) in enumerate(trainloader, 0):
-        x, y = x.to(parameters['device']), y.to(parameters['device'])
-        optimizer.zero_grad()
-        outputs = model.forward(x)
-        _, preds = torch.max(outputs, 1)
-        loss = criterion(outputs, y)
-        acc = (torch.sum(preds == y.data)) / len(x)
-
-        # Log batch loss
-        ns_run["training/batch/loss"].log(value = loss)
-
-        # Log batch accuracy
-        ns_run["training/batch/acc"].log(value = acc)
-
-        loss.backward()
-
-        # logging grad_norm
-        for p in list(filter(lambda p: p.grad is not None, model.parameters())): 
-            ns_run['training/grad_norm'].log((p.grad.data.norm(2).item()))
-
-        optimizer.step()
-    
-
-        running_loss += loss.item()
-        running_corrects += torch.sum(preds == y.data)
+    # Step 3: Log losses & metrics 
+    for epoch in range(params['epochs']):
+        running_loss = 0.0
+        running_corrects = 0
         
-    epoch_loss = running_loss/dataset_size['train']
-    epoch_acc = running_corrects.double().item() / dataset_size['train']
+        for i, (x, y) in enumerate(trainloader, 0):
 
-    # Log epoch loss
-    ns_run[f"training/epoch/loss"].log(value = epoch_loss, step = epoch + 1)
+            optimizer.zero_grad()
+            outputs = model.forward(x)
+            _, preds = torch.max(outputs, 1)
+            loss = criterion(outputs, y)
+            acc = (torch.sum(preds == y.data)) / len(x)
+            # Log batch loss
+            run["logs/training/batch/loss"].log(value = loss)
 
-    # Log epoch accuracy
-    ns_run[f"training/epoch/acc"].log(value = epoch_acc, step = epoch + 1)
+            # Log batch accuracy
+            run["logs/training/batch/acc"].log(value = acc)
+            loss.backward()
+            optimizer.step()
 
-    print(f'Epoch:{epoch+1}, Loss: {epoch_loss}, Acc: {epoch_acc}')
-    if epoch_acc > best_acc:
-        best_acc = epoch_acc
+            running_loss += loss.item()
+            running_corrects += torch.sum(preds == y.data)
+            
+        epoch_loss = running_loss/dataset_size['train']
+        epoch_acc = running_corrects.double().item() / dataset_size['train']
 
-        # Saving model arch & weights
-        save_model(model, parameters['model_filename'])
-        print('Saving model -- Done!')
+        # Log epoch loss
+        run[f"logs/training/epoch/loss"].log(value = epoch_loss, step = epoch + 1)
 
-# Log model arch & weights
-ns_run[f"io_files/artifacts/{parameters['model_filename']}_arch"].upload(f"./{parameters['model_filename']}_arch.txt")
-ns_run[f"io_files/artifacts/{parameters['model_filename']}"].upload(f"./{parameters['model_filename']}.pth")
+        # Log epoch accuracy
+        run[f"logs/training/epoch/acc"].log(value = epoch_acc, step = epoch + 1)
 
-# Explore results in Neptune UI
+        print(f'Epoch:{epoch+1}, Loss: {epoch_loss}, Acc: {epoch_acc}')
+    
+    # Step 5: Explore results in Neptune UI
 
-# Stop logging
-run.stop()
+    # Stop logging
+    run.stop()
+
+if __name__ == '__main__':
+    main()
