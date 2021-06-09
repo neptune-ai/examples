6b06ef1ac5efa83d96d84a723356172b1eef7d3f
Prince Canuma <prince.gdt@gmail.com>
2021-06-10 01:08:06 +0530

    :white_check_mark: :art: Basic example

--- integrations-and-supported-tools/fastai/scripts/Neptune_fastai.py
+++ integrations-and-supported-tools/fastai/scripts/Neptune_fastai.py
@@ -1,33 +1,16 @@
-
 import fastai
-
-fastai.__version__
-
 from neptune_fastai.impl import NeptuneCallback
 from fastai.vision.all import *
 import neptune.new as neptune
 
-"""run"""
-
-run = neptune.init(project='common/fastai-integration', api_token='ANONYMOUS')
-
-"""data"""
+run = neptune.init(project='common/fastai-integration', tags= 'basic', api_token='ANONYMOUS')
 
 path = untar_data(URLs.MNIST_TINY)
-
-path.ls()
-
 dls = ImageDataLoaders.from_csv(path)
 
-dls.show_batch()
-
-learn = cnn_learner(dls, resnet18, cbs=[NeptuneCallback(run, 'experiment')])
-
+neptune_cbk = NeptuneCallback(run, 'experiment')
+learn = cnn_learner(dls, resnet18, cbs=[neptune_cbk])
 learn.fit_one_cycle(1)
-
-learn.fine_tune(2)
-
-??NeptuneCallback()
 
 run.stop()
 
