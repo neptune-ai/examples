ea394c842281d46493aafdb5ea958008b9904c50
Prince Canuma <prince.gdt@gmail.com>
2021-06-11 19:34:39 +0530

    :sparkles: Single and multi phase logging examples

--- integrations-and-supported-tools/fastai/scripts/Neptune_fastai.py
+++ integrations-and-supported-tools/fastai/scripts/Neptune_fastai.py
@@ -9,8 +9,15 @@ path = untar_data(URLs.MNIST_TINY)
 dls = ImageDataLoaders.from_csv(path)
 
 neptune_cbk = NeptuneCallback(run, 'experiment')
+learn = cnn_learner(dls, resnet18)
+
+# Log a single training phase
+learn.fit(1, cbs=[neptune_cbk])
+
+# Log all training phases of the learner
 learn = cnn_learner(dls, resnet18, cbs=[neptune_cbk])
 learn.fit_one_cycle(1)
+learn.fit_one_cycle(2)
 
 run.stop()
 
