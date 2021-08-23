8cfefe5ff18794359b3521e979936217b88ad158
jakubczakon <czakon.jakub@gmail.com>
2021-08-23 08:32:00 +0200

    dropped advanced, added logging plot

--- integrations-and-supported-tools/kedro/scripts/run_examples.sh
+++ integrations-and-supported-tools/kedro/scripts/run_examples.sh
@@ -3,10 +3,6 @@ pip install -r requirements.txt
 
 echo "Running kedro_neptune_quickstart..."
 cd kedro_neptune_quickstart
-#kedro run
-
-echo "Running kedro_neptune_advanced..."
-cd ../kedro_neptune_advanced
 kedro run
 
 # Uncomment the next line to prevent execution window from closing when script execution is complete
