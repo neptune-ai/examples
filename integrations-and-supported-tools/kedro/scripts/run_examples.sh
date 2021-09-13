24308393c2b1149ab33760217c23bb3a9462675a
jakubczakon <czakon.jakub@gmail.com>
2021-09-13 21:09:29 +0200

    fixed kedro examples, added all files from kedro project

--- integrations-and-supported-tools/kedro/scripts/run_examples.sh
+++ integrations-and-supported-tools/kedro/scripts/run_examples.sh
@@ -1,12 +1,18 @@
 echo "Installing requirements..."
 pip install -r requirements.txt
 
+echo "Setting Neptune credentials..."
+export NEPTUNE_API_TOKEN=ANONYMOUS
+export NEPTUNE_PROJECT=common/kedro-integration
+
 echo "Running kedro_neptune_quickstart..."
 cd kedro_neptune_quickstart
+kedro neptune init --api-token $NEPTUNE_API_TOKEN --project $NEPTUNE_PROJECT
 kedro run
 
 echo "Running kedro_neptune_advanced..."
 cd ../kedro_neptune_advanced
+kedro neptune init --api-token $NEPTUNE_API_TOKEN --project $NEPTUNE_PROJECT
 kedro run
 
 # Uncomment the next line to prevent execution window from closing when script execution is complete
