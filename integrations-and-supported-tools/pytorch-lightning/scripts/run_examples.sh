dc6db0060b9d132f089cfaeba0599b323a646b5a
Kamil A. Kaczmarek <kamil.kaczmarek@neptune.ai>
2021-11-02 23:27:13 +0100

    PTL example (#58)
    * init
    
    * added comments
    
    * added more options example
    
    * added reqs
    
    * updated scripts
    
    * added notebook
    
    * droped

---
+++ integrations-and-supported-tools/pytorch-lightning/scripts/run_examples.sh
@@ -0,0 +1,8 @@
+echo "Installing requirements..."
+pip install -r requirements.txt
+
+echo "Running Neptune_Pytorch_Support.py..."
+python Neptune_Pytorch_Lightning.py
+
+echo "Running Neptune_Pytorch_Support_more_options.py..."
+python Neptune_Pytorch_Lightning_more_options.py
