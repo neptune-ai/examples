6c588d8da3a42395dabc923bed9cee66a4fc26a1
Prince Canuma <prince.gdt@gmail.com>
2021-11-23 16:14:58 +0530

    text formatting

--- how-to-guides/re-run-failed-training/scripts/run_examples.sh
+++ how-to-guides/re-run-failed-training/scripts/run_examples.sh
@@ -3,6 +3,3 @@ pip install -r requirements.txt
 
 echo "Running re_run_failed_training.py..."
 python re_run_failed_training.py
-
-# Uncomment the next line to prevent execution window from closing when script execution is complete
-# read -p "Press [Enter] to exit "
