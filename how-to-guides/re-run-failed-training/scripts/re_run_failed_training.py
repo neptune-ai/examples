de41cea2864dccc6b1f481d38e1d52e714cc3193
Prince Canuma <prince.gdt@gmail.com>
2021-11-23 16:14:21 +0530

    Text formatting

--- how-to-guides/re-run-failed-training/scripts/re_run_failed_training.py
+++ how-to-guides/re-run-failed-training/scripts/re_run_failed_training.py
@@ -6,9 +6,7 @@ import torch.optim as optim
 import os
 from torchvision import datasets, transforms
 
-"""## Step 1: Get Run ID
-You will get the Run ID of the failed run **programmatically**.
-"""
+# Step 1: Get Run ID
 
 # Fetch project
 project = neptune.get_project(name='common/showroom', api_token='ANONYMOUS')
@@ -24,15 +22,7 @@ failed_run_id = runs_table_df[runs_table_df['sys/failed']==True]['sys/id'].value
 
 print('Failed_run_id = ', failed_run_id)
 
-"""## Step 2: Resume run
-Use the neptune.init() method to:
-* Re-open a run using the ID you got from the previous step 
-* Re-open it in the `read-only` mode
-
-You use the `read-only` mode so the metadata previously logged to the run is not accidentally changed. Also, you can re-open a run as many times as needed.
-
-"""
-
+# Step 2: Resume failed run
 failed_run = neptune.init(
     project="common/showroom",
     api_token="ANONYMOUS",
@@ -40,52 +30,29 @@ failed_run = neptune.init(
     run=failed_run_id
 )
 
-"""## Step 3: Fetching and downloading data from Neptune
-
-Fetch metadata(dataset and hyperparameters) needed to re-run the training. Precisely, you will download the hyperparameters used in the failed run to instantiate a model with the same configuration and then you will download the dataset path to get the same dataset too.
-
-To do that:
-
-### Use the  [.download()](https://docs.neptune.ai/api-reference/field-types#.download-3) method to retrieve the [dataset artifact](https://docs.neptune.ai/api-reference/field-types#artifact) to your local disk:
-"""
-
+# Step 3: Fetching and downloading data from Neptune
 data_dir = 'data'
-
 failed_run['artifacts/dataset'].download(destination=data_dir)
-
-"""### Use the [.fetch()](https://docs.neptune.ai/api-reference/field-types#.fetch-1) method to retrieve hyperparameters:"""
 
 # fetching non-file values 
 failed_run_params = failed_run['config/hyperparameters'].fetch()
 
-"""## Step 4: Create a new run
-Create a new Neptune run that will be used to log metadata in the re-run session.
-"""
 
+# Step 4: Create a new run
 new_run = neptune.init(
     project="common/showroom",
     tags=['re-run', 'successful training'],
     api_token="ANONYMOUS"
 )
 
-"""## Step 5: Log new training metadata
-Now you can continue working and logging metadata to a brand new Run.
-You can log metadata using the [Neptune API Client](https://docs.neptune.ai/you-should-know/what-can-you-log-and-display).
-
-### Log copy of dataset artifact from failed_run to new run
-"""
-
+# Step 5: Log new training metadata
+# Now you can continue working and logging metadata to a brand new Run.
 new_run["artifacts/dataset"].assign(failed_run["artifacts/dataset"].fetch())
 
-"""### Log Hyperparameters from failed run to new run"""
-
+# Log Hyperparameters from failed run to new run
 new_run["config/hyperparameters"] = failed_run_params
 
-"""### Load Dataset and Model
-
-Dataset
-"""
-
+# Load Dataset and Model
 data_tfms = {
     "train": transforms.Compose(
         [
@@ -101,8 +68,7 @@ trainloader = torch.utils.data.DataLoader(
     trainset, batch_size=failed_run_params["bs"], shuffle=True, num_workers=0
 )
 
-"""Model"""
-
+# Model
 class BaseModel(nn.Module):
     def __init__(self, input_sz, hidden_dim, n_classes):
         super(BaseModel, self).__init__()
@@ -126,8 +92,7 @@ model = BaseModel(
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(model.parameters(), lr=failed_run_params["lr"])
 
-"""### Log losses and metrics"""
-
+# Log losses and metrics
 for i, (x, y) in enumerate(trainloader, 0):
     x, y = x.to(failed_run_params["device"]), y.to(failed_run_params["device"])
     optimizer.zero_grad()
@@ -143,10 +108,6 @@ for i, (x, y) in enumerate(trainloader, 0):
     loss.backward()
     optimizer.step()
 
-"""# Stop run
-Once you are done logging, you should stop tracking the run using the `stop()` method.
-This is needed only while logging from a notebook environment. While logging through a script, Neptune automatically stops tracking once the script has completed execution.
-"""
-
+# Stop run
 failed_run.stop()
 new_run.stop()
