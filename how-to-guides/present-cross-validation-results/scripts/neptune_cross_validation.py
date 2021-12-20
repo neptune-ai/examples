605ae9b5d3ab294d1fa15d7cfab5c22e3cc60087
Prince Canuma <prince.gdt@gmail.com>
2021-12-20 17:09:19 +0530

    code refactoring

--- how-to-guides/present-cross-validation-results/scripts/neptune_cross_validation.py
+++ how-to-guides/present-cross-validation-results/scripts/neptune_cross_validation.py
@@ -1,5 +1,4 @@
 # How to present CV with Neptune
-
 import neptune.new as neptune
 import numpy as np
 import torch
@@ -16,21 +15,23 @@ run = neptune.init(
 )
 
 # Step 2: Log config and hyperparameters
-
-# Log Hyperparameters
 parameters = {
-    "epochs": 2,
+    "epochs": 1,
     "lr": 1e-2,
     "bs": 10,
     "input_sz": 32 * 32 * 3,
     "n_classes": 10,
-    "k_folds": 5,
+    "k_folds": 2,
     "model_name": "checkpoint.pth",
     "device": torch.device("cuda:0" if torch.cuda.is_available() else "cpu"),
     "seed": 42,
 }
 
-run["global/params"] = parameters
+# Log hyperparameters
+run["global/parameters"] = parameters
+
+# Seed
+torch.manual_seed(parameters['seed'])
 
 # Model
 class BaseModel(nn.Module):
@@ -45,24 +46,17 @@ class BaseModel(nn.Module):
             nn.ReLU(),
             nn.Linear(hidden_dim // 2, n_classes),
         )
-
     def forward(self, input):
         x = input.view(-1, 32 * 32 * 3)
         return self.main(x)
-
-torch.manual_seed(parameters['seed'])
+        
 model = BaseModel(
     parameters["input_sz"], parameters["input_sz"], parameters["n_classes"]
 ).to(parameters["device"])
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(model.parameters(), lr=parameters["lr"])
 
-# Log model, criterion and optimizer name
-run["global/config/model"] = type(model).__name__
-run["global/config/criterion"] = type(criterion).__name__
-run["global/config/optimizer"] = type(optimizer).__name__
-
-# Dataset
+# trainset
 data_dir = "data/CIFAR10"
 compressed_ds = "./data/CIFAR10/cifar-10-python.tar.gz"
 data_tfms = {
@@ -72,107 +66,46 @@ data_tfms = {
             transforms.ToTensor(),
             transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
         ]
-    ),
-    "val": transforms.Compose(
-        [
-            transforms.ToTensor(),
-            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
-        ]
-    ),
+    )
 }
-
 trainset = datasets.CIFAR10(data_dir, transform=data_tfms["train"], download=True)
+dataset_size = len(trainset)
 
-validset = datasets.CIFAR10(
-    data_dir, train=False, transform=data_tfms["train"], download=True
-)
-
-dataset_size = {"train": len(trainset), "val": len(validset)}
-
-dataset = ConcatDataset([trainset, validset])
-
-## Log dataset details
 run["global/dataset/CIFAR-10"].track_files(data_dir)
 run["global/dataset/dataset_transforms"] = data_tfms
 run["global/dataset/dataset_size"] = dataset_size
 
+splits = KFold(n_splits=parameters['k_folds'], shuffle=True)
+epoch_acc_list, epoch_loss_list= [], []
 
-# Step 3: Log losses and metrics 
-def train_step(run, model,trainloader,loss_fn,optimizer,train=True):
-    epoch_loss,epoch_acc=0.0,0
-    if train: 
-        model.train() 
-    else:
-        model.eval()
+for fold, (train_ids, _ ) in enumerate(splits.split(trainset)):
+    train_sampler = SubsetRandomSampler(train_ids)
+    train_loader = DataLoader(trainset, batch_size=parameters['bs'], sampler=train_sampler)
+    for epoch in range(parameters["epochs"]): 
+        epoch_acc, epoch_loss= 0, 0.0
+        for x, y in train_loader:
+            x, y = x.to(parameters["device"]), y.to(parameters["device"])
+            optimizer.zero_grad()
+            outputs = model.forward(x)
+            _, preds = torch.max(outputs, 1)
+            loss = criterion(outputs, y)
+            acc = (torch.sum(preds == y.data)) / len(x)
 
-    for x, y in trainloader:
-        x, y = x.to(parameters["device"]), y.to(parameters["device"])
-        optimizer.zero_grad()
-        outputs = model.forward(x)
-        _, preds = torch.max(outputs, 1)
-        loss = criterion(outputs, y)
-        acc = (torch.sum(preds == y.data)) / len(x)
-
-        if train:
-            # log batch loss and acc
-            run["training/batch/loss"].log(loss)
-            run["training/batch/acc"].log(acc)
-
+            # Log batch loss and acc
+            run[f"fold_{fold}/training/batch/loss"].log(loss)
+            run[f"fold_{fold}/training/batch/acc"].log(acc)
+    
             loss.backward()
             optimizer.step()
-        else: 
-            # log batch loss and acc
-            run["validation/batch/loss"].log(loss)
-            run["validation/batch/acc"].log(acc)
-
+    
         epoch_acc += torch.sum(preds == y.data).item() 
         epoch_loss += loss.item() * x.size(0)
-
-    epoch_acc = (epoch_acc / len(train_loader.sampler)) * 100
-    epoch_loss = epoch_loss / len(train_loader.sampler)
-
-    return epoch_acc, epoch_loss
-
-splits = KFold(n_splits=parameters['k_folds'], shuffle=True)
-
-# K-fold training loop
-for fold, (train_idx,val_idx) in enumerate(splits.split(np.arange(len(dataset)))):
-
-    train_sampler = SubsetRandomSampler(train_idx)
-    test_sampler = SubsetRandomSampler(val_idx)
-    train_loader = DataLoader(dataset, batch_size=parameters['bs'], sampler=train_sampler)
-    test_loader = DataLoader(dataset, batch_size=parameters['bs'], sampler=test_sampler)
-
-    history = {
-        'train': 
-        {
-            'mean_loss': [], 
-            'mean_acc': []
-        }, 
-        'val': 
-        {
-            'mean_loss': [],
-            'mean_acc':[]
-        }
-    }
-
-    for epoch in range(parameters['epochs']):
-        train_acc, train_loss = train_step(run[f'fold_{fold}'],model,train_loader,criterion,optimizer)
-        val_acc, val_loss = train_step(run[f'fold_{fold}'],model,test_loader,criterion,optimizer,train=False)
-
-        history['train']['mean_loss'].append(train_loss)
-        history['train']['mean_acc'].append(train_acc)
-        history['val']['mean_loss'].append(val_loss)
-        history['val']['mean_acc'].append(val_acc)
-
-        # log model weights
-        torch.save(model.state_dict(), "./" + parameters["model_name"])
-        run[f'fold_{fold}/checkpoint'].upload(parameters['model_name'])
-
-history['train']['mean_loss'] = mean(history['train']['loss'])
-history['train']['mean_acc'] = mean(history['train']['acc'])
-history['val']['mean_loss'] = mean(history['val']['loss'])
-history['val']['mean_acc'] = mean(history['val']['acc'])
-
-# log global acc and loss
-run['global/metrics'] = history
+    epoch_acc_list.append((epoch_acc / len(train_loader.sampler)) * 100)
+    epoch_loss_list.append(epoch_loss / len(train_loader.sampler))
+     
+    # Log model checkpoint       
+    torch.save(model.state_dict(), f"./{parameters['model_name']}")
+    run[f'fold_{fold}/checkpoint'].upload(parameters['model_name'])
+    
+run["global/metrics/train/mean_acc"] = mean(epoch_acc_list)
+run["global/metrics/train/mean_loss"] = mean(epoch_loss_list)  
