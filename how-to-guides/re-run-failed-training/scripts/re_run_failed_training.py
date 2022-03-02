1040c539f66b710930e40db257e67101b814d3e6
Prince Canuma <prince.gdt@gmail.com>
2022-03-02 12:24:21 +0530

    text formatting

--- how-to-guides/re-run-failed-training/scripts/re_run_failed_training.py
+++ how-to-guides/re-run-failed-training/scripts/re_run_failed_training.py
@@ -5,8 +5,6 @@ import torch.optim as optim
 from torchvision import datasets, transforms
 from dotenv import load_dotenv
 
-load_dotenv('./examples/how-to-guides/re-run-failed-training/.env')
-
 # Step 1: Get Run ID
 ## Get project
 project = neptune.get_project(name="common/showroom", api_token="ANONYMOUS")
@@ -30,6 +28,9 @@ failed_run = neptune.init(
 # Step 3: Fetch relevant metadata from Neptune
 ## Use the download() method to retrieve the dataset artifact to your local disk
 data_dir = "data"
+
+## S3 bucket credentials
+load_dotenv('./examples/how-to-guides/re-run-failed-training/.env')
 
 ## Download tracked dataset files from S3 bucket
 failed_run["artifacts/dataset"].download(destination=data_dir)
