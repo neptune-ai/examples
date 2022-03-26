402c4d6e9e138a98b66993a44b94a76b3905929d
SiddhantSadangi <siddhant.sadangi@gmail.com>
2022-03-26 17:19:41 +0530

    Styleguide changes [Batch 1.1]: Changes applied to create_delete_projects and data_versioning; ALL code formatted with black

--- how-to-guides/create-delete-projects/scripts/create_delete_projects.py
+++ how-to-guides/create-delete-projects/scripts/create_delete_projects.py
@@ -1,11 +1,12 @@
 from neptune import management
 
-PROJECT_NAME = "common/YOUR_PROJECT"
-API_TOKEN = "ANONYMOUS"
+PROJECT_NAME = "YOUR_WORKSPACE/YOUR_PROJECT"
+API_TOKEN = "YOUR_API_TOKEN"
+KEY = "YOUR_PROJECT_KEY"
 
 # create project and choose whether the project should be public or private
 management.create_project(
-    name=PROJECT_NAME, api_token=API_TOKEN, key="AMA", visibility="pub"
+    name=PROJECT_NAME, api_token=API_TOKEN, key=KEY, visibility="pub"
 )
 
 # list projects that you have access to
