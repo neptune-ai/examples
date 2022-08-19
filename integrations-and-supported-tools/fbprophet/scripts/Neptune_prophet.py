251ea080d135cc345d22854e4183cc69e011d71a
Prince Canuma <prince.gdt@gmail.com>
2022-08-19 22:07:27 +0200

    refactor

--- integrations-and-supported-tools/fbprophet/scripts/Neptune_prophet.py
+++ integrations-and-supported-tools/fbprophet/scripts/Neptune_prophet.py
@@ -18,10 +18,7 @@ df["cap"] = 8.5
 
 def nfl_sunday(ds):
     date = pd.to_datetime(ds)
-    if date.weekday() == 6 and (date.month > 8 or date.month < 2):
-        return 1
-    else:
-        return 0
+    return 1 if date.weekday() == 6 and (date.month > 8 or date.month < 2) else 0
 
 
 df["nfl_sunday"] = df.ds.apply(nfl_sunday)
