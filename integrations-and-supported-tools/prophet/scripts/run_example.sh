66cc4e36f115847126a7a349d414b305babc9c3d
Siddhant Sadangi <siddhant.sadangi@gmail.com>
2022-08-24 10:13:52 +0530

    Unlocked neptune-prophet version, removed plotly and matplotlib

--- integrations-and-supported-tools/prophet/scripts/run_example.sh
+++ integrations-and-supported-tools/prophet/scripts/run_example.sh
@@ -1,4 +1,8 @@
-set -e
+echo "Installing requirements..."
 pip install -r requirements.txt
+
+echo "Running Neptune_prophet.pys..."
 python Neptune_prophet.py
+
+echo "Running Neptune_prophet_more_options.pys..."
 python Neptune_prophet_more_options.py
