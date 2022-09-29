ee18e700355a2e532baa98fb7db1a0889a6e8c8e
kshitij12345 <kshitijkalambarkar@gmail.com>
2022-09-29 11:44:38 +0000

    update bash script

--- integrations-and-supported-tools/transformers/scripts/run_examples.sh
+++ integrations-and-supported-tools/transformers/scripts/run_examples.sh
@@ -7,3 +7,6 @@ export NEPTUNE_PROJECT=common/huggingface-integration
 
 echo "Running Neptune_Transformers.py..."
 python Neptune_Transformers.py
+
+echo "Running Neptune_Transformers_report_to.py..."
+python Neptune_Transformers_report_to.py
