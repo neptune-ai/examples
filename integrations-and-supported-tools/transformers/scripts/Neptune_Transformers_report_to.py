c9c00713ebf46e647b380217e2e40a07439e8543
Kshiteej K <kshitijkalambarkar@gmail.com>
2022-09-30 09:05:33 +0530

    add example nb for hf transformers integration (#118)
    * add example nb for hf transformers integration
    
    * update dependencies
    
    * address reviews
    
    * update README.md
    
    * fix pre-commit hook
    
    * add script
    
    * add requirements and run_examples
    
    * update depenencies
    
    * add colab link
    
    * add Neptune Link to README
    
    * Increased PR tests timeout to 15 mins
    
    * Added transformers to tests
    
    * Increased timeout to 20 mins
    
    * use bert-tiny
    
    * update model
    
    * update dependency
    
    * update dependency
    
    * update dependency
    
    * update dependency
    
    * Add numpy dep as it is not installed automatically on windows and python 3.10
    
    * change numpy version
    
    * Updated requirements, removed step timeouts
    
    * Apply suggestions from code review
    
    * address review: update project and demo the option of logging with report_to
    
    * add script with report_to
    
    * update script
    
    * update notebook link
    
    * Fixed PR test scripts action
    
    * Removed redundant steps from PR test scripts action, added test scripts step for windows
    
    * Added os condition to test scripts
    
    * Updated get changed files filter
    
    * update bash script
    
    * remove stray line
    
    Co-authored-by: Siddhant Sadangi <siddhant.sadangi@neptune.ai>
    Co-authored-by: Sabine <aksiomi@protonmail.com>

---
+++ integrations-and-supported-tools/transformers/scripts/Neptune_Transformers_report_to.py
@@ -0,0 +1,52 @@
+import neptune.new as neptune
+from datasets import load_dataset, load_metric
+from transformers import (
+    AutoModelForSequenceClassification,
+    AutoTokenizer,
+    Trainer,
+    TrainingArguments,
+)
+
+task = "cola"
+model_checkpoint = "prajjwal1/bert-tiny"
+batch_size = 16
+dataset = load_dataset("glue", task)
+metric = load_metric("glue", task)
+num_labels = 2
+
+tokenizer = AutoTokenizer.from_pretrained(model_checkpoint, use_fast=True)
+
+
+def preprocess_function(examples):
+    return tokenizer(examples["sentence"], truncation=True)
+
+
+encoded_dataset = dataset.map(preprocess_function, batched=True)
+
+model = AutoModelForSequenceClassification.from_pretrained(model_checkpoint, num_labels=num_labels)
+model_name = model_checkpoint.split("/")[-1]
+
+args = TrainingArguments(
+    f"{model_name}-finetuned-{task}",
+    evaluation_strategy="epoch",
+    save_strategy="epoch",
+    learning_rate=2e-5,
+    per_device_train_batch_size=batch_size,
+    per_device_eval_batch_size=batch_size,
+    num_train_epochs=2,
+    weight_decay=0.01,
+    load_best_model_at_end=True,
+    report_to="neptune",
+)
+
+validation_key = "validation"
+
+trainer = Trainer(
+    model,
+    args,
+    train_dataset=encoded_dataset["train"],
+    eval_dataset=encoded_dataset[validation_key],
+    tokenizer=tokenizer,
+)
+
+trainer.train()
---
+++ integrations-and-supported-tools/transformers/scripts/run_examples.sh
@@ -0,0 +1,12 @@
+echo "Installing requirements..."
+pip install -r requirements.txt
+
+echo "Setting Neptune credentials..."
+export NEPTUNE_API_TOKEN=ANONYMOUS
+export NEPTUNE_PROJECT=common/huggingface-integration
+
+echo "Running Neptune_Transformers.py..."
+python Neptune_Transformers.py
+
+echo "Running Neptune_Transformers_report_to.py..."
+python Neptune_Transformers_report_to.py
