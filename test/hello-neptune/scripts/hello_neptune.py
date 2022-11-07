234e20235abc5aeeff3dbcb5ddb5d1434910eb1f
Siddhant Sadangi <siddhant.sadangi@gmail.com>
2022-11-07 10:04:54 +0530

    test

--- test/hello-neptune/scripts/hello_neptune.py
+++ test/hello-neptune/scripts/hello_neptune.py
@@ -1,14 +1,1 @@
-from time import sleep
-
-import neptune.new as neptune
-import numpy as np
-
-run = neptune.init(project="common/quickstarts", api_token="ANONYMOUS")
-
-# log score
-run["single_metric"] = 0.62
-
-for i in range(100):
-    sleep(0.2)  # to see logging live
-    run["random_training_metric"].log(i * np.random.random())
-    run["other_random_training_metric"].log(0.5 * i * np.random.random())
+print("test")
