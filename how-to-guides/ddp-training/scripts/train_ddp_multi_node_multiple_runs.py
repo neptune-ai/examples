5b0ed5e94765cdb0d8a040969683396df07a7c50
Prince Canuma <prince.gdt@gmail.com>
2022-11-26 12:36:40 +0100

    fix double api

--- how-to-guides/ddp-training/scripts/train_ddp_multi_node_multiple_runs.py
+++ how-to-guides/ddp-training/scripts/train_ddp_multi_node_multiple_runs.py
@@ -169,7 +169,6 @@ if __name__ == "__main__":
     # by passing the `custom_run_id` argument
     run = neptune.init_run(
         project="common/showroom",
-        api_token=neptune.ANONYMOUS_API_TOKEN,
         monitoring_namespace=monitoring_namespace,
         custom_run_id=custom_run_id,
     )
