b0f625b674387cbd481a2b548f40fead01420271
kshitij12345 <kshitijkalambarkar@gmail.com>
2022-12-09 09:37:22 +0000

    linter

--- integrations-and-supported-tools/tensorflow/scripts/Neptune_Tensorflow.py
+++ integrations-and-supported-tools/tensorflow/scripts/Neptune_Tensorflow.py
@@ -10,9 +10,9 @@ run = neptune.init_run(
     project="common/quickstarts",
 )
 
-response = requests.get('https://storage.googleapis.com/tensorflow/tf-keras-datasets/mnist.npz')
-with open('mnist.npz', 'wb') as f:
-  f.write(response.content)
+response = requests.get("https://storage.googleapis.com/tensorflow/tf-keras-datasets/mnist.npz")
+with open("mnist.npz", "wb") as f:
+    f.write(response.content)
 
 # (Neptune) Track and version data files used for training
 run["data/details"].track_files("mnist.npz")
