26d0c22b4594a90e3fa74e18d3111be023ce5b0f
Kshiteej K <kshitijkalambarkar@gmail.com>
2022-12-09 14:46:01 +0530

    update run_examples.sh

--- integrations-and-supported-tools/tensorflow/scripts/run_examples.sh
+++ integrations-and-supported-tools/tensorflow/scripts/run_examples.sh
@@ -4,5 +4,5 @@ pip install -r requirements.txt
 echo "Downloading data..."
 wget 'https://storage.googleapis.com/tensorflow/tf-keras-datasets/mnist.npz'
 
-echo "Running Neptune_TensorFlow_Keras.py..."
-python Neptune_TensorFlow.py
+echo "Running Neptune_Tensorflow.py..."
+python Neptune_Tensorflow.py
