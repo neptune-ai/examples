a3c940bd87cab8d995baa5a2e8eef00feeda541d
Tymoteusz Wołodźko <twolodzko+git@gmail.com>
2022-12-28 09:33:19 +0100

    Improve the build script

--- integrations-and-supported-tools/sagemaker/build_and_push.sh
+++ integrations-and-supported-tools/sagemaker/build_and_push.sh
@@ -1,37 +1,30 @@
 #!/bin/bash
 set -e
 
-# The name of our algorithm
 algorithm_name="neptune-sagemaker-demo"
-
-chmod +x decision_trees/train
-
 account=$(aws sts get-caller-identity --query Account --output text)
+chmod +x decision_trees/train
 
 # Get the region defined in the current configuration (default to us-west-2 if none defined)
 region=$(aws configure get region)
-region=${region:-us-west-2}
+region="${region:-us-west-2}"
 
-fullname="${account}.dkr.ecr.${region}.amazonaws.com/${algorithm_name}:latest"
-echo "Image: ${fullname}"
+full_name="${account}.dkr.ecr.${region}.amazonaws.com/${algorithm_name}:latest"
+echo "Image: ${full_name}"
 
 # If the repository doesn't exist in ECR, create it.
-aws ecr describe-repositories --repository-names "${algorithm_name}" > /dev/null 2>&1
-
-if [ $? -ne 0 ]
+if ! aws ecr describe-repositories --repository-names "${algorithm_name}" > /dev/null
 then
     aws ecr create-repository --repository-name "${algorithm_name}" > /dev/null
 fi
 
 # Get the login command from ECR and execute it directly
-aws ecr get-login-password --region "${region}" | docker login --username AWS --password-stdin "${fullname}"
+aws ecr get-login-password --region "${region}" | docker login --username AWS --password-stdin "${full_name}"
 
 # Build the docker image locally with the image name and then push it to ECR
 # with the full name.
-
 docker build -t ${algorithm_name} .
-docker tag "${algorithm_name}" "${fullname}"
-
-docker push "${fullname}"
+docker tag "${algorithm_name}" "${full_name}"
+docker push "${full_name}"
 
 echo "Success!"
