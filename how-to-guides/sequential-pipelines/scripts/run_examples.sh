b2ce15519bf8e193e3a509b11c5bc59cc712a6c1
Prince Canuma <prince.gdt@gmail.com>
2023-01-16 14:47:45 +0100

    add model training and evaluation scripts

--- how-to-guides/sequential-pipelines/scripts/run_examples.sh
+++ how-to-guides/sequential-pipelines/scripts/run_examples.sh
@@ -7,5 +7,7 @@ echo "Running data_preprocessing.py..."
 python data_preprocessing.py
 
 echo "Running model_training.py"
+python model_training.py
 
-echo "Running training.py"
+echo "Running model_evalution.py"
+python model_evalution.py
