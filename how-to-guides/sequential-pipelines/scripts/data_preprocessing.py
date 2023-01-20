fb76f94044715f84cd232ccfab3e2eb96b4d273b
Prince Canuma <prince.gdt@gmail.com>
2023-01-20 13:36:16 +0100

    add neptune

--- how-to-guides/sequential-pipelines/scripts/data_preprocessing.py
+++ how-to-guides/sequential-pipelines/scripts/data_preprocessing.py
@@ -1,10 +1,44 @@
+import neptune.new as neptune
 from sklearn.datasets import fetch_lfw_people
 from utils import *
 
 # Download dataset
 dataset = fetch_lfw_people(min_faces_per_person=70, resize=0.4)
 
+# (Neptune) Create a new run
+run = neptune.init_run(
+    project="common/showroom",
+    api_token=neptune.ANONYMOUS_API_TOKEN,
+    monitoring_namespace="monitoring/preprocessing",
+)
+
+# Get dataset details
+dataset_config = {
+    "target_names": str(dataset.target_names.tolist()),
+    "n_classes": dataset.target_names.shape[0],
+    "n_samples": dataset.images.shape[0],
+    "height": dataset.images.shape[1],
+    "width": dataset.images.shape[2],
+}
+
+# (Neptune) Set basenamespace
+handler_run = run["preprocessing"]
+
+# (Neptune) Log dataset details
+handler_run["dataset/config"] = dataset_config
+
 # Preprocess dataset
-data_transform = Preprocessing(dataset=dataset)
-data_transform.scale_features()
-data_transform.describe()
+dataset_transform = Preprocessing(
+    dataset,
+    dataset_config["n_samples"],
+    dataset_config["target_names"],
+    dataset_config["n_classes"],
+    (dataset_config["height"], dataset_config["width"]),
+)
+path_to_scaler = dataset_transform.scale_data()
+path_to_features = dataset_transform.create_and_save_features(data_filename="features")
+dataset_transform.describe()
+
+# (Neptune) Log scaler and features files
+handler_run["dataset/scaler"].upload(path_to_scaler)
+handler_run["dataset/features"].upload(path_to_features)
