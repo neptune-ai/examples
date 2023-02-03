c4748bfd60f608854c28d8336d62daf432c1919f
Kshiteej K <kshitijkalambarkar@gmail.com>
2023-02-03 22:10:43 +0530

    Update integrations-and-supported-tools/detectron2/scripts/run_examples.sh
    Co-authored-by: Siddhant Sadangi <siddhant.sadangi@neptune.ai>
    Signed-off-by: Kshiteej K <kshitijkalambarkar@gmail.com>

--- integrations-and-supported-tools/detectron2/scripts/run_examples.sh
+++ integrations-and-supported-tools/detectron2/scripts/run_examples.sh
@@ -4,7 +4,7 @@ echo "Installing requirements..."
 pip install -U -r requirements.txt
 pip install -U 'git+https://github.com/facebookresearch/detectron2.git'
 
-echo "Download and unzip the dataset"
+echo "Downloading and unzipping the dataset"
 wget https://github.com/matterport/Mask_RCNN/releases/download/v2.1/balloon_dataset.zip
 unzip balloon_dataset.zip > /dev/null
 
