2adce9a4f2681903ba754072884ec50f7b8e398f
kshitij12345 <kshitijkalambarkar@gmail.com>
2023-02-03 05:01:34 +0000

    script

---
+++ integrations-and-supported-tools/detectron2/scripts/Neptune_detectron2.py
@@ -0,0 +1,147 @@
+import json
+import os
+import random
+
+import cv2
+import detectron2
+import neptune.new as neptune
+
+# import some common libraries
+import numpy as np
+import torch
+
+# import some common detectron2 utilities
+from detectron2 import model_zoo
+from detectron2.config import get_cfg
+from detectron2.data import DatasetCatalog, MetadataCatalog
+from detectron2.engine import DefaultPredictor, DefaultTrainer
+from detectron2.structures import BoxMode
+from detectron2.utils.logger import setup_logger
+from detectron2.utils.visualizer import ColorMode, Visualizer
+from neptune_detectron2 import NeptuneHook
+
+setup_logger()
+
+# (Neptune) Initialize a new run
+run = neptune.init_run(
+    api_token=neptune.ANONYMOUS_API_TOKEN,
+    project="common/detectron2-integration",
+)
+
+# ### Transform dataset for training
+
+# step code
+# if your dataset is in COCO format, this cell can be replaced by the following three lines:
+# from detectron2.data.datasets import register_coco_instances
+# register_coco_instances("my_dataset_train", {}, "json_annotation_train.json", "path/to/image/dir")
+# register_coco_instances("my_dataset_val", {}, "json_annotation_val.json", "path/to/image/dir")
+
+
+def get_balloon_dicts(img_dir):
+    json_file = os.path.join(img_dir, "via_region_data.json")
+    with open(json_file) as f:
+        imgs_anns = json.load(f)
+
+    dataset_dicts = []
+    for idx, v in enumerate(imgs_anns.values()):
+        record = {}
+
+        filename = os.path.join(img_dir, v["filename"])
+        height, width = cv2.imread(filename).shape[:2]
+
+        record["file_name"] = filename
+        record["image_id"] = idx
+        record["height"] = height
+        record["width"] = width
+
+        annos = v["regions"]
+        objs = []
+        for _, anno in annos.items():
+            assert not anno["region_attributes"]
+            anno = anno["shape_attributes"]
+            px = anno["all_points_x"]
+            py = anno["all_points_y"]
+            poly = [(x + 0.5, y + 0.5) for x, y in zip(px, py)]
+            poly = [p for x in poly for p in x]
+
+            obj = {
+                "bbox": [np.min(px), np.min(py), np.max(px), np.max(py)],
+                "bbox_mode": BoxMode.XYXY_ABS,
+                "segmentation": [poly],
+                "category_id": 0,
+            }
+            objs.append(obj)
+        record["annotations"] = objs
+        dataset_dicts.append(record)
+    return dataset_dicts
+
+
+for d in ["train", "val"]:
+    DatasetCatalog.register("balloon_" + d, lambda d=d: get_balloon_dicts("balloon/" + d))
+    MetadataCatalog.get("balloon_" + d).set(thing_classes=["balloon"])
+balloon_metadata = MetadataCatalog.get("balloon_train")
+
+
+# Configure the model for training
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+cfg = get_cfg()
+cfg.MODEL.DEVICE = device
+cfg.DATALOADER.NUM_WORKERS = 0
+cfg.merge_from_file(
+    model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")
+)
+cfg.DATASETS.TRAIN = ("balloon_train",)
+cfg.DATASETS.TEST = ()
+cfg.DATALOADER.NUM_WORKERS = 2
+cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url(
+    "COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"
+)  # Let training initialize from model zoo
+cfg.SOLVER.IMS_PER_BATCH = 2  # This is the real "batch size" commonly known to deep learning people
+cfg.SOLVER.BASE_LR = 0.00025  # pick a good LR
+cfg.SOLVER.MAX_ITER = 30  # 30 iterations seems good enough for this toy dataset; you will need to train longer for a practical dataset
+cfg.SOLVER.STEPS = []  # do not decay learning rate
+cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 128  # The "RoIHead batch size". 128 is faster, and good enough for this toy dataset (default: 512)
+cfg.MODEL.ROI_HEADS.NUM_CLASSES = 1  # only has one class (ballon). (see https://detectron2.readthedocs.io/tutorials/datasets.html#update-the-config-for-new-datasets)
+# NOTE: this config means the number of classes, but a few popular unofficial tutorials incorrect uses num_classes+1 here.
+
+
+# (Neptune) Create a hook using Neptune integration
+hook = NeptuneHook(run=run, log_checkpoints=True, log_model=True)
+
+
+# (Neptune) Train the model with hook
+os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
+trainer = DefaultTrainer(cfg)
+trainer.resume_or_load(resume=False)
+trainer.register_hooks([hook])
+trainer.train()
+
+
+# Prepare model for prediction
+cfg.MODEL.WEIGHTS = os.path.join(
+    cfg.OUTPUT_DIR, "model_final.pth"
+)  # path to the model we just trained
+cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.7  # set a custom testing threshold
+predictor = DefaultPredictor(cfg)
+
+
+# (Neptune) Log model predictions to neptune
+dataset_dicts = get_balloon_dicts("balloon/val")
+for idx, d in enumerate(random.sample(dataset_dicts, 3)):
+    im = cv2.imread(d["file_name"])
+    outputs = predictor(
+        im
+    )  # format is documented at https://detectron2.readthedocs.io/tutorials/models.html#model-output-format
+    v = Visualizer(
+        im[:, :, ::-1],
+        metadata=balloon_metadata,
+        scale=0.5,
+        instance_mode=ColorMode.IMAGE,  # remove the colors of unsegmented pixels. This option is only available for segmentation models
+    )
+    out = v.draw_instance_predictions(outputs["instances"].to("cpu"))
+    image = out.get_image()[:, :, ::-1]
+    img_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    run[f"training/prediction_visualization/{idx}"].upload(
+        neptune.types.File.as_image(img_rgb / 255.0)
+    )
---
+++ integrations-and-supported-tools/detectron2/scripts/run_examples.sh
@@ -0,0 +1,12 @@
+set -e
+
+echo "Installing requirements..."
+pip install -U -r requirements.txt
+pip install -U 'git+https://github.com/facebookresearch/detectron2.git'
+
+echo "Download and unzip the dataset"
+wget https://github.com/matterport/Mask_RCNN/releases/download/v2.1/balloon_dataset.zip
+unzip balloon_dataset.zip > /dev/null
+
+echo "Running Neptune_detectron2.py..."
+python Neptune_detectron2.py
