f2edfa94b56a803fcfc93cc2eee330fa7bb5c88b
Siddhant Sadangi <siddhant.sadangi@gmail.com>
2023-02-15 18:38:48 +0100

    Moving run_examples.sh for test compatibility

--- use-cases/time-series-forecasting/walmart-sales/scripts/run_examples.sh
+++ use-cases/time-series-forecasting/walmart-sales/scripts/run_examples.sh
@@ -1,16 +1,16 @@
 set -e
 
 echo "Installing requirements..."
-pip install -U -r requirements.txt
+pip install -U -r ../requirements.txt
 
 echo "Running run_ml_baseline.py..."
-python ./scripts/run_ml_baseline.py
+python run_ml_baseline.py
 
 echo "Running run_ml_prophet.py..."
-python ./scripts/run_ml_prophet.py
+python run_ml_prophet.py
 
 echo "Running run_dl_lstm.py..."
-python ./scripts/run_dl_lstm.py
+python run_dl_lstm.py
 
 echo "Running run_dl_lstm_finetune.py..."
-python ./scripts/run_dl_lstm_finetune.py
+python run_dl_lstm_finetune.py
