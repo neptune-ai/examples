52bd68b71b661080ed165ae7af131195c5d7f3e4
Siddhant Sadangi <siddhant.sadangi@gmail.com>
2023-02-24 12:37:57 +0100

    Updated kedro run_examples.sh

--- integrations-and-supported-tools/kedro/scripts/run_examples.sh
+++ integrations-and-supported-tools/kedro/scripts/run_examples.sh
@@ -3,15 +3,12 @@ set -e
 echo "Installing requirements..."
 pip install -U -r requirements.txt
 
-# echo "Setting Neptune credentials..."
-# export NEPTUNE_PROJECT=common/kedro-integration
-
 echo "Running kedro-neptune-quickstart..."
 cd kedro-neptune-quickstart
 kedro neptune init --api-token $NEPTUNE_API_TOKEN --project common/kedro-integration
 kedro run
 
-# echo "Running kedro-neptune-advanced..."
-# cd ../kedro-neptune-advanced
-# kedro neptune init --api-token $NEPTUNE_API_TOKEN --project $NEPTUNE_PROJECT
-# kedro run
+echo "Running kedro-neptune-advanced..."
+cd ../kedro-neptune-advanced
+kedro neptune init --api-token $NEPTUNE_API_TOKEN --project common/kedro-integration
+kedro run
