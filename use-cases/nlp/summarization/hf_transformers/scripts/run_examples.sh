8126d0ba90d0d7a2e3c3b6a67543c4ead5b53e51
Siddhant Sadangi <siddhant.sadangi@gmail.com>
2023-03-08 18:20:37 +0100

    Tweaked summarization parameters for faster tests

--- use-cases/nlp/summarization/hf_transformers/scripts/run_examples.sh
+++ use-cases/nlp/summarization/hf_transformers/scripts/run_examples.sh
@@ -6,21 +6,20 @@ pip install -U -r requirements.txt --user
 echo "Running run_summarization.py..."
 python run_summarization.py \
     --s3_path "s3://neptune-examples/data/samsum/data_v1/" \
-    --learning_rate 5e-4 \
+    --learning_rate 5e-3 \
     --num_train_epochs 2 \
-    --max_target_length 100 \
+    --max_target_length 50 \
     --max_eval_samples 10 \
     --model_name_or_path google/t5-efficient-tiny \
     --dataset_name samsum \
     --dataset_config samsum \
     --report_to "none" \
     --output_dir "../models/" \
-    --per_device_train_batch_size=8 \
+    --per_device_train_batch_size=16 \
     --per_device_eval_batch_size=2 \
     --overwrite_output_dir \
     --load_best_model_at_end true \
     --evaluation_strategy "epoch" \
     --save_strategy "epoch" \
-    --eval_accumulation_steps 100 \
     --include_inputs_for_metrics true \
     --neptune_project "common/project-text-summarization-hf"
