6c47173f173995f3a0f0f9896edfa6fd4eeaa324
Prince Canuma <prince.gdt@gmail.com>
2023-03-14 17:48:52 +0100

    update checkout shell script

--- how-to-guides/reproduce-run/scripts/run_git_checkout.sh
+++ how-to-guides/reproduce-run/scripts/run_git_checkout.sh
@@ -3,8 +3,8 @@ set -e
 echo "Installing requirements..."
 pip install -U -r requirements.txt
 
-echo "Checkout to commit ID ee906dbc6a5f3f9f5cf06e49edb005cc2d6d2c75 ..."
-git checkout ee906dbc6a5f3f9f5cf06e49edb005cc2d6d2c75
+echo "Checkout to commit ID 146841173bb1f05b00c587f689695d041fdc55c3 ..."
+git checkout 146841173bb1f05b00c587f689695d041fdc55c3
 
 echo "Running old_run.py..."
 python old_run.py
