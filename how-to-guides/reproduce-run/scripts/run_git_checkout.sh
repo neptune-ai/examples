77a997bc7701a971971f599b31a79caf6ebe95b8
Prince Canuma <prince.gdt@gmail.com>
2023-03-14 17:05:22 +0100

    check commit id

--- how-to-guides/reproduce-run/scripts/run_git_checkout.sh
+++ how-to-guides/reproduce-run/scripts/run_git_checkout.sh
@@ -3,8 +3,8 @@ set -e
 echo "Installing requirements..."
 pip install -U -r requirements.txt
 
-echo "Checkout to commit ID 42ff89ed836b8240cce582eec7e7840bc412ebfd ..."
-git checkout 42ff89ed836b8240cce582eec7e7840bc412ebfd
+echo "Checkout to commit ID b6ea01d27dd4c10fec12f4d49220ee34cc03fb3c ..."
+git checkout b6ea01d27dd4c10fec12f4d49220ee34cc03fb3c
 
 echo "Running old_run.py..."
 python old_run.py
