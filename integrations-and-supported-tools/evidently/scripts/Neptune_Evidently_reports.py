39686cdc7eede01c8e89e98a4834f70580b2d9db
Siddhant Sadangi <siddhant.sadangi@gmail.com>
2023-05-05 14:12:48 +0200

    Added Evidently example

---
+++ integrations-and-supported-tools/evidently/scripts/Neptune_Evidently_reports.py
@@ -0,0 +1,56 @@
+import neptune
+from evidently.metric_preset import DataDriftPreset
+from evidently.report import Report
+from evidently.test_preset import DataStabilityTestPreset
+from evidently.test_suite import TestSuite
+from neptune.utils import stringify_unsupported
+from sklearn import datasets
+
+# Load sample data
+iris_data = datasets.load_iris(as_frame="auto")
+iris_frame = iris_data.frame
+
+# Run Evidently test suites and reports
+data_stability = TestSuite(
+    tests=[
+        DataStabilityTestPreset(),
+    ]
+)
+data_stability.run(
+    current_data=iris_frame.iloc[:60],
+    reference_data=iris_frame.iloc[60:],
+    column_mapping=None,
+)
+
+data_drift_report = Report(
+    metrics=[
+        DataDriftPreset(),
+    ]
+)
+data_drift_report.run(
+    current_data=iris_frame.iloc[:60],
+    reference_data=iris_frame.iloc[60:],
+    column_mapping=None,
+)
+
+# (Neptune) Start a run
+run = neptune.init_run(
+    api_token=neptune.ANONYMOUS_API_TOKEN,
+    project="common/evidently-support",
+    tags=["reports"],  # (optional) replace with your own
+)
+
+# (Neptune) Save and upload reports as HTML
+data_stability.save_html("data_stability.html")
+data_drift_report.save_html("data_drift_report.html")
+
+run["data_stability/report"].upload("data_stability.html")
+run["data_drift/report"].upload("data_drift_report.html")
+
+# (Neptune) Save reports as dict
+
+run["data_stability"] = stringify_unsupported(data_stability.as_dict())
+run["data_drift"] = stringify_unsupported(data_drift_report.as_dict())
+
+# (Neptune) Stop logging (optional for scripts)
+run.stop()
