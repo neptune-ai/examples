fb1dc9ab27787b7807a4b145e0c8dfba4be8528a
Kshiteej K <kshitijkalambarkar@gmail.com>
2023-05-10 21:24:55 +0530

    Pytorch example (#224)
    * update pytorch example
    
    * update nb requirements
    
    * updates
    
    * update deps
    
    * update
    
    * update
    
    * updates
    
    * update readme
    
    * update example run
    
    * Update integrations-and-supported-tools/pytorch/notebooks/Neptune_PyTorch.ipynb
    
    Co-authored-by: Sabine <sabine.nyholm@neptune.ai>
    Signed-off-by: Kshiteej K <kshitijkalambarkar@gmail.com>
    
    * Update integrations-and-supported-tools/pytorch/notebooks/Neptune_PyTorch.ipynb
    
    Co-authored-by: Sabine <sabine.nyholm@neptune.ai>
    Signed-off-by: Kshiteej K <kshitijkalambarkar@gmail.com>
    
    * Update integrations-and-supported-tools/pytorch/notebooks/Neptune_PyTorch.ipynb
    
    Co-authored-by: Sabine <sabine.nyholm@neptune.ai>
    Signed-off-by: Kshiteej K <kshitijkalambarkar@gmail.com>
    
    * Update integrations-and-supported-tools/pytorch/scripts/Neptune_Pytorch.py
    
    Co-authored-by: Sabine <sabine.nyholm@neptune.ai>
    Signed-off-by: Kshiteej K <kshitijkalambarkar@gmail.com>
    
    * Update integrations-and-supported-tools/pytorch/scripts/Neptune_Pytorch.py
    
    Co-authored-by: Sabine <sabine.nyholm@neptune.ai>
    Signed-off-by: Kshiteej K <kshitijkalambarkar@gmail.com>
    
    * Update integrations-and-supported-tools/pytorch/scripts/requirements.txt
    
    Co-authored-by: Sabine <sabine.nyholm@neptune.ai>
    Signed-off-by: Kshiteej K <kshitijkalambarkar@gmail.com>
    
    * update nb
    
    * Update integrations-and-supported-tools/pytorch/notebooks/Neptune_PyTorch.ipynb
    
    Co-authored-by: Sabine <sabine.nyholm@neptune.ai>
    Signed-off-by: Kshiteej K <kshitijkalambarkar@gmail.com>
    
    * Update integrations-and-supported-tools/pytorch/notebooks/Neptune_PyTorch.ipynb
    
    Co-authored-by: Sabine <sabine.nyholm@neptune.ai>
    Signed-off-by: Kshiteej K <kshitijkalambarkar@gmail.com>
    
    * Update integrations-and-supported-tools/pytorch/notebooks/Neptune_PyTorch.ipynb
    
    Co-authored-by: Sabine <sabine.nyholm@neptune.ai>
    Signed-off-by: Kshiteej K <kshitijkalambarkar@gmail.com>
    
    * adddress review
    
    * Updated pip install. Added saved model and checkpoints to showcase run
    
    ---------
    
    Signed-off-by: Kshiteej K <kshitijkalambarkar@gmail.com>
    Co-authored-by: Sabine <sabine.nyholm@neptune.ai>
    Co-authored-by: Siddhant Sadangi <siddhant.sadangi@neptune.ai>

--- integrations-and-supported-tools/pytorch/scripts/run_examples.sh
+++ integrations-and-supported-tools/pytorch/scripts/run_examples.sh
@@ -3,8 +3,5 @@ set -e
 echo "Installing requirements..."
 pip install -U -r requirements.txt
 
-echo "Running Neptune_Pytorch_Support.py..."
-python Neptune_Pytorch_Support.py
-
-echo "Running Neptune_Pytorch_Support_more_options.py..."
-python Neptune_Pytorch_Support_more_options.py
+echo "Running Neptune_Pytorch.py..."
+python Neptune_Pytorch.py
