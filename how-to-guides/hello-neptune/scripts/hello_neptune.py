2b6de5da93f8a63abed24a83f57e79bd80825676
Siddhant Sadangi <siddhant.sadangi@gmail.com>
2023-06-02 18:02:34 +0200

    Updated quickstart

--- how-to-guides/hello-neptune/scripts/hello_neptune.py
+++ how-to-guides/hello-neptune/scripts/hello_neptune.py
@@ -1,14 +1,48 @@
+import neptune
+
+# Initialize Neptune and create a new run
+run = neptune.init_run(
+    project="common/quickstarts",
+    api_token=neptune.ANONYMOUS_API_TOKEN,
+    tags=["quickstart", "script"],
+)
+
+# log single value
+run["seed"] = 0.42
+
+# log series of values
+from random import random
 from time import sleep
 
-import neptune
-import numpy as np
+epochs = 10
+offset = random() / 5
 
-run = neptune.init_run(project="common/quickstarts", api_token=neptune.ANONYMOUS_API_TOKEN)
-
-# log score
-run["single_metric"] = 0.62
-
-for i in range(100):
+for epoch in range(epochs):
     sleep(0.2)  # to see logging live
-    run["random_training_metric"].append(i * np.random.random())
-    run["other_random_training_metric"].append(0.5 * i * np.random.random())
+    acc = 1 - 2**-epoch - random() / (epoch + 1) - offset
+    loss = 2**-epoch + random() / (epoch + 1) + offset
+
+    run["accuracy"].append(acc)
+    run["loss"].append(loss)
+
+# Upload single image to Neptune
+run["single_image"].upload("Lenna_test_image.png")  # Native images can be upload as-is
+
+# Download MNIST dataset
+from keras.datasets import mnist
+
+(train_X, train_y), (test_X, test_y) = mnist.load_data()
+
+# Upload a series of images to Neptune
+from neptune.types import File
+
+for i in range(10):
+    run["image_series"].append(
+        File.as_image(
+            train_X[i] / 255
+        ),  # Arrays can be uploaded as images using Neptune's `File.as_image()` method
+        name=f"{train_y[i]}",
+    )
+
+# Stop logging
+run.stop()
