d36201e677d484756a8e5ed0a4e074638379b16b
Siddhant Sadangi <siddhant.sadangi@neptune.ai>
2023-06-06 10:54:41 +0200

    Updated quickstart (#231)
    * Updated quickstart
    
    * Added keras to requirements
    
    * Removed keras dependency
    
    * Apply suggestions from code review
    
    Co-authored-by: Sabine <sabine.nyholm@neptune.ai>
    Signed-off-by: Siddhant Sadangi <siddhant.sadangi@gmail.com>
    
    * Removed conclusion
    
    * Uncommented api token from script
    
    * Comment on source_files in colab
    
    ---------
    
    Signed-off-by: Siddhant Sadangi <siddhant.sadangi@gmail.com>
    Co-authored-by: Sabine <sabine.nyholm@neptune.ai>

--- how-to-guides/hello-neptune/scripts/hello_neptune.py
+++ how-to-guides/hello-neptune/scripts/hello_neptune.py
@@ -1,14 +1,47 @@
-from time import sleep
-
 import neptune
-import numpy as np
 
-run = neptune.init_run(project="common/quickstarts", api_token=neptune.ANONYMOUS_API_TOKEN)
+# Initialize Neptune and create a new run
+run = neptune.init_run(
+    project="common/quickstarts",
+    api_token=neptune.ANONYMOUS_API_TOKEN,
+    tags=["quickstart", "script"],
+)
 
-# log score
-run["single_metric"] = 0.62
+# log single value
+run["seed"] = 0.42
 
-for i in range(100):
-    sleep(0.2)  # to see logging live
-    run["random_training_metric"].append(i * np.random.random())
-    run["other_random_training_metric"].append(0.5 * i * np.random.random())
+# log series of values
+from random import random
+
+epochs = 10
+offset = random() / 5
+
+for epoch in range(epochs):
+    acc = 1 - 2**-epoch - random() / (epoch + 1) - offset
+    loss = 2**-epoch + random() / (epoch + 1) + offset
+
+    run["accuracy"].append(acc)
+    run["loss"].append(loss)
+
+# Upload single image to Neptune
+run["single_image"].upload("Lenna_test_image.png")  # You can upload native images as-is
+
+# Download MNIST dataset
+import mnist
+
+train_images = mnist.train_images()
+train_labels = mnist.train_labels()
+
+# Upload a series of images to Neptune
+from neptune.types import File
+
+for i in range(10):
+    run["image_series"].append(
+        File.as_image(
+            train_images[i] / 255
+        ),  # You can upload arrays as images using Neptune's File.as_image() method
+        name=f"{train_labels[i]}",
+    )
+
+# Stop logging
+run.stop()
--- how-to-guides/hello-neptune/scripts/run_examples.sh
+++ how-to-guides/hello-neptune/scripts/run_examples.sh
@@ -4,4 +4,5 @@ echo "Installing requirements..."
 pip install -U -r requirements.txt
 
 echo "Running hello_neptune.py..."
+curl -O https://neptune.ai/wp-content/uploads/2023/06/Lenna_test_image.png
 python hello_neptune.py
