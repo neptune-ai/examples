0cbf83239a0bec3c1c26ba53b04096da9401fe27
Siddhant Sadangi <siddhant.sadangi@gmail.com>
2023-06-08 13:00:03 +0200

    [WIP] Updated lightning example

--- integrations-and-supported-tools/pytorch-lightning/scripts/Neptune_Pytorch_Lightning.py
+++ integrations-and-supported-tools/pytorch-lightning/scripts/Neptune_Pytorch_Lightning.py
@@ -3,8 +3,8 @@ import os
 import neptune
 import numpy as np
 import torch
-from pytorch_lightning import LightningModule, Trainer
-from pytorch_lightning.loggers import NeptuneLogger
+from lightning import LightningModule, Trainer
+from lightning.pytorch.loggers import NeptuneLogger
 from sklearn.metrics import accuracy_score
 from torch.nn import functional as F
 from torch.utils.data import DataLoader
@@ -23,12 +23,13 @@ params = {
 class MNISTModel(LightningModule):
     def __init__(self):
         super().__init__()
+        self.training_step_outputs = []
         self.l1 = torch.nn.Linear(28 * 28, 10)
 
     def forward(self, x):
         return torch.relu(self.l1(x.view(x.size(0), -1)))
 
-    def training_step(self, batch, batch_idx):
+    def training_step(self, batch):
         x, y = batch
         y_hat = self(x)
         loss = F.cross_entropy(y_hat, y)
@@ -39,19 +40,22 @@ class MNISTModel(LightningModule):
         acc = accuracy_score(y_true, y_pred)
         self.log("metrics/batch/acc", acc)
 
+        self.training_step_outputs.append({"loss": loss, "y_true": y_true, "y_pred": y_pred})
+
         return {"loss": loss, "y_true": y_true, "y_pred": y_pred}
 
-    def training_epoch_end(self, outputs):
+    def on_train_epoch_end(self):
         loss = np.array([])
         y_true = np.array([])
         y_pred = np.array([])
-        for results_dict in outputs:
-            loss = np.append(loss, results_dict["loss"])
+        for results_dict in self.training_step_outputs:
+            loss = np.append(loss, results_dict["loss"].detach().numpy())
             y_true = np.append(y_true, results_dict["y_true"])
             y_pred = np.append(y_pred, results_dict["y_pred"])
         acc = accuracy_score(y_true, y_pred)
         self.log("metrics/epoch/loss", loss.mean())
         self.log("metrics/epoch/acc", acc)
+        self.training_step_outputs.clear()  # free memory
 
     def configure_optimizers(self):
         return torch.optim.Adam(self.parameters(), lr=params["lr"])
