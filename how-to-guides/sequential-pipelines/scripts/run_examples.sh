186ec3fe045ce520d8408022c488d6db0cc005c1
Siddhant Sadangi <siddhant.sadangi@gmail.com>
2023-06-12 17:03:09 +0200

    Updated NEPTUNE_CUSTOM_RUN_ID

--- how-to-guides/sequential-pipelines/scripts/run_examples.sh
+++ how-to-guides/sequential-pipelines/scripts/run_examples.sh
@@ -1,6 +1,6 @@
 set -e
 
-export NEPTUNE_CUSTOM_RUN_ID=`(date +%s) | md5`
+export NEPTUNE_CUSTOM_RUN_ID=date +%s
 export NEPTUNE_PROJECT="common/showroom"
 
 echo "Installing requirements..."
@@ -12,8 +12,8 @@ python data_preprocessing.py
 echo "Running model_training.py"
 python model_training.py
 
-echo "Running model_validation.py"
-python model_validation.py
+# echo "Running model_validation.py"
+# python model_validation.py
 
-echo "Running model_promotion.py"
-python model_promotion.py
+# echo "Running model_promotion.py"
+# python model_promotion.py
