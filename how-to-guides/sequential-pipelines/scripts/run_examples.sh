fec9d7541141f8f965595954aad11ced6c5d1196
Siddhant Sadangi <siddhant.sadangi@gmail.com>
2023-06-12 16:42:00 +0200

    Fixed actions yaml

--- how-to-guides/sequential-pipelines/scripts/run_examples.sh
+++ how-to-guides/sequential-pipelines/scripts/run_examples.sh
@@ -9,11 +9,11 @@ pip install -U -r requirements.txt
 echo "Running data_preprocessing.py..."
 python data_preprocessing.py
 
-# echo "Running model_training.py"
-# python model_training.py
+echo "Running model_training.py"
+python model_training.py
 
-# echo "Running model_validation.py"
-# python model_validation.py
+echo "Running model_validation.py"
+python model_validation.py
 
-# echo "Running model_promotion.py"
-# python model_promotion.py
+echo "Running model_promotion.py"
+python model_promotion.py
