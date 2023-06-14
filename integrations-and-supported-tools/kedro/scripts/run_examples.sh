7ed5a40e0b14bb3a41b9f60a67c8040b5c3bfec6
Siddhant Sadangi <siddhant.sadangi@gmail.com>
2023-06-14 17:55:54 +0200

    Updated run_examples.sh

--- integrations-and-supported-tools/kedro/scripts/run_examples.sh
+++ integrations-and-supported-tools/kedro/scripts/run_examples.sh
@@ -5,10 +5,10 @@ pip install -U -r requirements.txt
 
 echo "Running kedro-neptune-quickstart..."
 cd kedro-neptune-quickstart
-kedro neptune init --project common/kedro-integration --api-token "ANONYMOUS" # Replace with your own token or use $NEPTUNE_API_TOKEN
+kedro neptune init --project common/kedro-integration --api-token "ANONYMOUS" # Replace with your own token and project
 kedro run
 
 echo "Running kedro-neptune-advanced..."
 cd ../kedro-neptune-advanced
-kedro neptune init --project common/kedro-integration --api-token "ANONYMOUS" # Replace with your own token or use $NEPTUNE_API_TOKEN
+kedro neptune init --project common/kedro-integration --api-token "ANONYMOUS" # Replace with your own token and project
 kedro run
