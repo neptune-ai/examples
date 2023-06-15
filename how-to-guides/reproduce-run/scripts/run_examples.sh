12b80ea1e4bdb2ad5b1cd66c5f5a3ddfe9025809
Siddhant Sadangi <siddhant.sadangi@neptune.ai>
2023-06-15 10:06:03 +0200

    Commented `git checkout ...`. Updated README.md (#236)

--- how-to-guides/reproduce-run/scripts/run_examples.sh
+++ how-to-guides/reproduce-run/scripts/run_examples.sh
@@ -6,5 +6,9 @@ pip install -U -r requirements.txt
 echo "Running reproduce_run.py..."
 python reproduce_run.py
 
-echo "Running run_git_checkout.sh ..."
-sh run_git_checkout.sh
+# You can get the commit ID to checkout to in source_code/git namespace of the run
+# echo "Checkout to commit ID 146841173bb1f05b00c587f689695d041fdc55c3 ..."
+# git checkout 146841173bb1f05b00c587f689695d041fdc55c3
+
+echo "Running old_run.py..."
+python old_run.py
