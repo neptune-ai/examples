ae5a5daef1323c8799b627d6c04abf78b6c8b40e
Siddhant Sadangi <siddhant.sadangi@gmail.com>
2023-06-16 16:07:35 +0200

    Added plotly to requirements

--- integrations-and-supported-tools/prophet/scripts/run_examples.sh
+++ integrations-and-supported-tools/prophet/scripts/run_examples.sh
@@ -6,5 +6,5 @@ pip install -U -r requirements.txt
 echo "Running Neptune_prophet.py..."
 python Neptune_prophet.py
 
-# echo "Running Neptune_prophet_more_options.py..."
-# python Neptune_prophet_more_options.py
+echo "Running Neptune_prophet_more_options.py..."
+python Neptune_prophet_more_options.py
