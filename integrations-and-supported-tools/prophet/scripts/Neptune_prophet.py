f6b0d116a0e3147d64c834d9d8642dbb40f83db8
Siddhant Sadangi <siddhant.sadangi@gmail.com>
2023-06-16 15:58:01 +0200

    Testing with `log_interactive=True`

--- integrations-and-supported-tools/prophet/scripts/Neptune_prophet.py
+++ integrations-and-supported-tools/prophet/scripts/Neptune_prophet.py
@@ -30,4 +30,6 @@ model.fit(df)
 
 forecast = model.predict(df)
 
-run["prophet_summary"] = npt_utils.create_summary(model=model, df=df, fcst=forecast)
+run["prophet_summary"] = npt_utils.create_summary(
+    model=model, df=df, fcst=forecast, log_interactive=True
+)
