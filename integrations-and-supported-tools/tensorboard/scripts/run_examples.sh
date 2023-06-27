ad18bf78aac890e413e3928056fe22444cf6623e
Siddhant Sadangi <siddhant.sadangi@neptune.ai>
2023-06-27 21:51:21 +0530

    Update run_examples.sh
    Signed-off-by: Siddhant Sadangi <siddhant.sadangi@neptune.ai>

--- integrations-and-supported-tools/tensorboard/scripts/run_examples.sh
+++ integrations-and-supported-tools/tensorboard/scripts/run_examples.sh
@@ -6,5 +6,5 @@ pip install -U -r requirements.txt
 echo "Running Neptune_Tensorflow_Tensorboard.py..."
 python Neptune_Tensorflow_Tensorboard.py
 
-echo "Export previous logs..."
+echo "Exporting previous logs..."
 neptune tensorboard logs --project common/tensorboard-integration
