b779527948715a414662619d50b49a13adfb5226
kshitij12345 <kshitijkalambarkar@gmail.com>
2023-08-02 19:02:07 +0000

    updates

--- integrations-and-supported-tools/airflow/scripts/run_examples.sh
+++ integrations-and-supported-tools/airflow/scripts/run_examples.sh
@@ -1,7 +1,10 @@
-# set -e
+set -e
 
 echo "Installing requirements..."
-pip install -U -r requirements.txt --no-cache-dir
+pip install -U -r requirements.txt
+
+echo "Initialize Airflow DB..."
+airflow initdb
 
 echo "Running Neptune_Airflow.py..."
 python Neptune_Airflow.py
