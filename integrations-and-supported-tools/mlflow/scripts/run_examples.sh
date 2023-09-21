e16ce0ae0fe159d6d7d068700b86a569f4edd1d3
Siddhant Sadangi <siddhant.sadangi@gmail.com>
2023-09-21 14:23:16 +0200

    [Draft] MLflow plugin example

--- integrations-and-supported-tools/mlflow/scripts/run_examples.sh
+++ integrations-and-supported-tools/mlflow/scripts/run_examples.sh
@@ -3,11 +3,20 @@ set -e
 echo "Installing requirements..."
 pip install -U -r requirements.txt
 
+#TODO: Update installation after neptune-mlflow release
+git clone https://github.com/neptune-ai/neptune-mlflow
+pip install -e "neptune-mlflow"
+
+echo "Creating sample MLflow runs for export..."
+
 echo "Running train_keras_mlflow.py..."
 python train_keras_mlflow.py
 
 echo "Running train_scikit_mlflow.py..."
 python train_scikit_mlflow.py
 
-echo "Export MLflow runs to Neptune..."
+echo "Exporting MLflow runs to Neptune..."
 neptune mlflow --project common/mlflow-integration
+
+echo "Tracking MLflow run in Neptune using Neptune tracking URI..."
+python mlflow_neptune_plugin.py
