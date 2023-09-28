4638dcca348d43e67233b38a277e4239aea40ab1
Siddhant Sadangi <siddhant.sadangi@gmail.com>
2023-09-28 16:40:21 +0200

    Formatting

--- integrations-and-supported-tools/mosaicml-composer/scripts/Neptune_MosaicML_Composer.py
+++ integrations-and-supported-tools/mosaicml-composer/scripts/Neptune_MosaicML_Composer.py
@@ -2,22 +2,19 @@
 
 ## Import dependencies
 import torch
-
+from composer import Trainer
+from composer.algorithms import BlurPool, LabelSmoothing, ProgressiveResizing
+from composer.loggers import NeptuneLogger
+from composer.models import mnist_model
 from neptune import ANONYMOUS_API_TOKEN
 from neptune.types import File
-
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
-from composer import Trainer
-from composer.models import mnist_model
-from composer.loggers import NeptuneLogger
-from composer.algorithms import LabelSmoothing, BlurPool, ProgressiveResizing
-
 
 ## Prepare dataset and dataloaders
 transform = transforms.Compose([transforms.ToTensor()])
-train_dataset = datasets.MNIST('data', download=True, train=True, transform=transform)
-eval_dataset = datasets.MNIST('data', download=True, train=False, transform=transform)
+train_dataset = datasets.MNIST("data", download=True, train=True, transform=transform)
+eval_dataset = datasets.MNIST("data", download=True, train=False, transform=transform)
 train_dataloader = DataLoader(train_dataset, batch_size=128)
 eval_dataloader = DataLoader(eval_dataset, batch_size=128)
 
@@ -38,7 +35,7 @@ blurpool = BlurPool(
 )
 
 prog_resize = ProgressiveResizing(
-    initial_scale=.6,
+    initial_scale=0.6,
     finetune_fraction=0.34,
 )
 
@@ -49,15 +46,19 @@ trainer = Trainer(
     eval_dataloader=eval_dataloader,
     max_duration="3ep",
     device="gpu" if torch.cuda.is_available() else "cpu",
-    algorithms = [label_smoothing, blurpool, prog_resize],
+    algorithms=[label_smoothing, blurpool, prog_resize],
     loggers=neptune_logger,
 )
 
 trainer.fit()
 
 ## Log additional metadata to your custom namespaces
-neptune_logger.neptune_run[neptune_logger._base_namespace]["images/training"].extend([File.as_image(img/255) for img in train_dataset.data[:50]])
-neptune_logger.neptune_run[neptune_logger._base_namespace]["images/eval"].extend([File.as_image(img/255) for img in eval_dataset.data[:50]])
+neptune_logger.neptune_run["images/training"].extend(
+    [File.as_image(img / 255) for img in train_dataset.data[:50]]
+)
+neptune_logger.neptune_run["images/eval"].extend(
+    [File.as_image(img / 255) for img in eval_dataset.data[:50]]
+)
 
 ## Stop logging
 trainer.close()
@@ -65,4 +66,5 @@ trainer.close()
 ## Analyze run in the Neptune app
 # Follow the run link in the console output and explore the logged metadata.
 # You can also explore this example run
+# TODO: Update link
 # https://app.neptune.ai/o/common/org/mosaicml/runs/details?viewId=standard-view&detailsTab=dashboard&dashboardId=Overview-99f571df-0fec-4447-9ffe-5a4c668577cd&shortId=CAT-2
