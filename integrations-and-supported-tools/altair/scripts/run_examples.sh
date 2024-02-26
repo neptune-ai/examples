173febcdd0aca314cc8e35d1ef605624f7bcbbb5
Siddhant Sadangi <siddhant.sadangi@gmail.com>
2024-02-26 17:50:42 +0100

    Removed separate plotting examples

--- integrations-and-supported-tools/altair/scripts/run_examples.sh
+++
@@ -1,7 +0,0 @@
-set -e
-
-echo "Installing requirements..."
-pip install -U -q -r requirements.txt
-
-echo "Running Neptune_Altair_Support.py..."
-python Neptune_Altair_Support.py
--- integrations-and-supported-tools/bokeh/scripts/run_examples.sh
+++
@@ -1,7 +0,0 @@
-set -e
-
-echo "Installing requirements..."
-pip install -U -r requirements.txt
-
-echo "Running Neptune_Bokeh_Support.py..."
-python Neptune_Bokeh_Support.py
