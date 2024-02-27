0e59a0818d9537f7eacc1373bfa4a0146922379e
Siddhant Sadangi <siddhant.sadangi@neptune.ai>
2024-02-27 14:15:48 +0100

    Removed separate plotting examples (#297)

--- integrations-and-supported-tools/matplotlib/scripts/run_examples.sh
+++
@@ -1,7 +0,0 @@
-set -e
-
-echo "Installing requirements..."
-pip install -U -r requirements.txt --user
-
-echo "Running Neptune_Matplotlib_Support.py..."
-python Neptune_Matplotlib_Support.py
--- integrations-and-supported-tools/plotly/scripts/run_examples.sh
+++
@@ -1,7 +0,0 @@
-set -e
-
-echo "Installing requirements..."
-pip install -U -r requirements.txt
-
-echo "Running Neptune_Plotly_Support.py..."
-python Neptune_Plotly_Support.py
