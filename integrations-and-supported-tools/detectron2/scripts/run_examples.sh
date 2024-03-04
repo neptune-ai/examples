b3d9194ac2fc7f849d92a017a43e84aec32daa2b
Siddhant Sadangi <siddhant.sadangi@gmail.com>
2024-03-04 12:00:42 +0100

    Fixed bash error

--- integrations-and-supported-tools/detectron2/scripts/run_examples.sh
+++ integrations-and-supported-tools/detectron2/scripts/run_examples.sh
@@ -7,6 +7,7 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
 CC=clang CXX=clang++ ARCHFLAGS="-arch x86_64" python -m pip install -U 'git+https://github.com/facebookresearch/detectron2.git' --no-build-isolation
 else
 pip install -U 'git+https://github.com/facebookresearch/detectron2.git' --no-build-isolation
+fi
 
 echo "Downloading and unzipping the dataset"
 wget https://github.com/matterport/Mask_RCNN/releases/download/v2.1/balloon_dataset.zip
