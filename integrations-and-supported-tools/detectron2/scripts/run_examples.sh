3371fb32fda08d9b827a87afcda9ac484f981a55
Siddhant Sadangi <siddhant.sadangi@neptune.ai>
2024-03-04 12:12:27 +0100

    Fix detectron2 installation issues on MacOS (#300)
    * Fix detectron2 installation issues on MacOS
    
    * Fixed bash error

--- integrations-and-supported-tools/detectron2/scripts/run_examples.sh
+++ integrations-and-supported-tools/detectron2/scripts/run_examples.sh
@@ -2,7 +2,12 @@ set -e
 
 echo "Installing requirements..."
 pip install -U -r requirements.txt
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+CC=clang CXX=clang++ ARCHFLAGS="-arch x86_64" python -m pip install -U 'git+https://github.com/facebookresearch/detectron2.git' --no-build-isolation
+else
 pip install -U 'git+https://github.com/facebookresearch/detectron2.git' --no-build-isolation
+fi
 
 echo "Downloading and unzipping the dataset"
 wget https://github.com/matterport/Mask_RCNN/releases/download/v2.1/balloon_dataset.zip
