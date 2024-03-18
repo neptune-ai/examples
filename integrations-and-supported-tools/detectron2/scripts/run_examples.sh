3b246f032457d93967e991b1bcfcde75b8acf5e4
Siddhant Sadangi <siddhant.sadangi@gmail.com>
2024-03-18 10:24:23 +0100

    Hopefully fixing detectron2 installation issue on macos for python>=3.11

--- integrations-and-supported-tools/detectron2/scripts/run_examples.sh
+++ integrations-and-supported-tools/detectron2/scripts/run_examples.sh
@@ -4,8 +4,12 @@ echo "Installing requirements..."
 pip install -U -r requirements.txt
 
 echo "Installing detectron2..."
+python -m pip install ninja
 if [[ "$OSTYPE" == "darwin"* ]]; then
-CC=clang CXX=clang++ ARCHFLAGS="-arch x86_64" python -m pip install -U 'git+https://github.com/facebookresearch/detectron2.git' --no-build-isolation
+export GRPC_PYTHON_BUILD_SYSTEM_OPENSSL=1
+export GRPC_PYTHON_BUILD_SYSTEM_ZLIB=1
+pip install --pre torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/nightly/cpu
+CC=clang CXX=clang++ ARCHFLAGS="-arch x86_64" python -m pip install -U 'git+https://github.com/facebookresearch/detectron2.git'
 else
 pip install -U 'git+https://github.com/facebookresearch/detectron2.git' --no-build-isolation
 fi
