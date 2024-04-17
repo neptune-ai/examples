bf0a05ecc2d004aa15ef5bf096ce59c17a9d0b6e
Siddhant Sadangi <siddhant.sadangi@gmail.com>
2024-04-17 11:48:21 +0200

    Suppressing pip warning

--- how-to-guides/e2e-tracking/scripts/run_examples.sh
+++ how-to-guides/e2e-tracking/scripts/run_examples.sh
@@ -1,7 +1,7 @@
 set -e
 
 echo "Installing requirements..."
-pip install --user -q -U -r requirements.txt
+pip install --user -q -U -r requirements.txt --no-warn-script-location
 
 echo "Running e2e_tracking.py..."
 python e2e_tracking.py
