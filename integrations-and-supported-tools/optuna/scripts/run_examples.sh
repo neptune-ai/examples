01c0669dd8f651ebf2f68fec27363ed227a1ac2a
Siddhant Sadangi <siddhant.sadangi@gmail.com>
2024-05-23 17:01:50 +0200

    Updated wrapper script

--- integrations-and-supported-tools/optuna/scripts/run_examples.sh
+++ integrations-and-supported-tools/optuna/scripts/run_examples.sh
@@ -1,13 +1,10 @@
 set -e
 
 echo "Installing requirements..."
-pip install -U -r requirements.txt
+pip install -q -U -r requirements.txt
 
 echo "Running Neptune_Optuna_integration_quickstart.py..."
 python Neptune_Optuna_integration_quickstart.py
-
-echo "Running Neptune_Optuna_integration_customize_callback.py..."
-python Neptune_Optuna_integration_customize_callback.py
 
 echo "Running Neptune_Optuna_integration_log_after_study.py..."
 python Neptune_Optuna_integration_log_after_study.py
