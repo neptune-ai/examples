e466a31d0647aeefe14e790fda80716e7aa9032d
Siddhant Sadangi <siddhant.sadangi@gmail.com>
2024-06-05 09:25:10 +0200

    Updated kedro project

--- integrations-and-supported-tools/kedro/scripts/run_examples.sh
+++ integrations-and-supported-tools/kedro/scripts/run_examples.sh
@@ -5,5 +5,5 @@ pip install -U -r requirements.txt
 
 echo "Running Kedro spaceflights..."
 cd spaceflights-pandas
-kedro neptune init --project common/kedro-integration --api-token "ANONYMOUS" # Replace with your own token and project
+kedro neptune init --project common/kedro --api-token "ANONYMOUS" # Replace with your own token and project
 kedro run
