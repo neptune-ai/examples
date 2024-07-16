787cd03fbb6ec173b7cdc7752261bafe12b2c758
Siddhant Sadangi <siddhant.sadangi@neptune.ai>
2024-07-16 11:28:57 +0200

    Ss/mr migration (#334)
    * feat: Added MR to Runs migration script
    
    * nit: Update README
    
    * chore: Refactor
    
    * feat: Add error handling to copy functions in model_to_run.py
    
    * Single-process modularization
    
    * Parallelized script
    
    * List to Set
    
    * chore: Improved readme
    
    * feat: Added MR to Runs migration script
    
    * nit: Update README
    
    * chore: Refactor
    
    * feat: Add error handling to copy functions in model_to_run.py
    
    * Single-process modularization
    
    * Parallelized script
    
    * List to Set
    
    * chore: Improved readme
    
    * feat: Improved README, added more metadata to the script
    
    * Update README.md
    
    Signed-off-by: Siddhant Sadangi <siddhant.sadangi@neptune.ai>
    
    * Update README.md
    
    Signed-off-by: Siddhant Sadangi <siddhant.sadangi@neptune.ai>
    
    * Update README.md
    
    Signed-off-by: Siddhant Sadangi <siddhant.sadangi@neptune.ai>
    
    * Apply suggestions from code review
    
    Co-authored-by: Edyta <142720610+szaganek@users.noreply.github.com>
    Signed-off-by: Siddhant Sadangi <siddhant.sadangi@gmail.com>
    
    * doc: Update README
    
    * doc: Updated README
    
    * Apply suggestions from code review
    
    Co-authored-by: Edyta <142720610+szaganek@users.noreply.github.com>
    Signed-off-by: Siddhant Sadangi <siddhant.sadangi@gmail.com>
    
    ---------
    
    Signed-off-by: Siddhant Sadangi <siddhant.sadangi@neptune.ai>
    Signed-off-by: Siddhant Sadangi <siddhant.sadangi@gmail.com>
    Co-authored-by: Edyta <142720610+szaganek@users.noreply.github.com>

---
+++ utils/migration_tools/model_registry_to_runs/README.md
@@ -0,0 +1,80 @@
+# Migrating from the Model Registry to Experiments
+
+This script allows you to copy models and model versions from the model registry to experiments, within the same project.
+
+## Why Migrate?
+Our revamped web app adds a host of new features to experiments, including better grouping and enhanced reporting capabilities. By migrating your models and model versions to experiments, you can take advantage of these features to better organize and compare your models.
+
+As a part of our focus on performance this year, we have also highly optimized the `Run` API  for speed. Using the `Run` object instead of `Model` or `ModelVersion` objects will allow you to take advantage of these optimizations.
+
+This script leverages the new *Group Tags* to organize your models and model versions in experiments, and *tags* to create saved views separating model metadata runs from training runs. As a result, you can view all models and model versions in the same table:
+
+![Models and model versions displayed in the Experiments tab in the Neptune app](https://neptune.ai/wp-content/uploads/2024/07/MRtoRun.png)
+
+In this screenshot, _Group Tag_, _Custom Run ID_, and _ID_ correspond to the original Model ID, Model Version ID and the new Run ID respectively.
+
+Having all model metadata in experiments also lets you use experiment's native [comparisons](https://docs.neptune.ai/usage/tutorial/#compare-the-runs) and [reports](https://docs.neptune.ai/app/reports/) to compare models and model versions.
+
+
+## Prerequisites
+
+Before using this script, make sure you have the Neptune environment variables set up. For instructions, see the [documentation](https://docs.neptune.ai/setup/setting_credentials/).
+
+## Instructions
+
+To use the script, follow these steps:
+
+1. Execute `model_to_run.py`.
+2. Enter the name of a project from which you want to copy the model metadata. Use the `WORKSPACE_NAME/PROJECT_NAME` format. To use the `NEPTUNE_PROJECT` environment variable, leave this prompt blank.
+3. Enter the number of workers to use to copy the metadata. Leave blank to use all available CPUs.
+4. The script will generate run logs in the same folder as `model_to_run.py`. You can modify this location by editing the `logging.basicConfig()` function.
+
+
+## Note
+
+There are a few things to keep in mind when using this script:
+
+- All models and model versions will be copied. Filtering is currently not available†.
+- Most of the namespaces from the model/model_versions will be retained in the runs, except for the following:
+  - `sys` namespace:
+    - The `state` field cannot be copied.
+    - The `description`, `name`, and `tags` fields are copied to the `sys` namespace in the new run.
+    - All other fields are copied to a new `old_sys` namespace in the new run.
+- The _Model Stage_ is currently copied to `old_sys/stage` field. Unlike the `sys/stage` field, this field cannot be updated from the web app. If you want to be able to update the _Model Stage_ from the web app, the script can be modified to copy the stage as _Tags_ instead†.
+- File metadata is temporarily stored in the current directory in a `tmp_%Y%m%d%H%M%S` folder. This folder is deleted after the script finishes running.
+- The relative time x-axis in copied charts is based on the `sys/creation_time` of the original runs. Since this field is read-only, the relative time will be negative in the copied charts, as the logging time occurred before the creation time of the new run.
+- The hash of tracked artifacts may change between the original and new runs.
+- Each file copied as a `FileSet` will have its file name prefixed with the namespace where it was stored in the original run. For example, if the original run has a file named `hello_neptune.py` stored in the `source_code/files` namespace, the corresponding file in the new run will be named `source_code/files/hello_neptune.py`.
+
+† Can be added based on feedback
+
+## Post-Migration
+- The source object of a run can be identified using `sys/custom_run_id`.
+- The Model ID of each model and corresponding model versions is stored as a group tag in the run to allow you to group models and model versions together, as shown in the screenshot above.
+- Runs made from models and model versions have the the `model` and `model_version` tags added respectively.
+- Once the migration and any sanity checks are complete, the copied Model/Model Versions can be deleted from the model registry to reclaim space.
+- This script can also be used a template to update your logging script to start logging model metadata to runs instead of the model registry.
+
+## Performance Benchmarks
+
+The script was tested on a project with 86 models and model versions with a total size of 510MB spread across metrics and files.  
+Using 20 workers on an internet connection with a download and upload speed of 340Mbps and 110Mbps respectively, and an average round-trip time to the Neptune server of 28ms, the entire migration took ~60 seconds.
+
+## Roadmap
+
+- [ ] Copy models and model versions from multiple or all projects of the workspace
+- [ ] Filter models and model versions to copy
+- [ ] Copy the _Model Stage_ as a tag instead of a field
+
+## Support and Feedback
+
+This script is currently in beta, and we welcome your feedback and contributions to help improve it. Please submit any issues or feature requests as [GitHub Issues](https://github.com/neptune-ai/examples/issues)
+
+## License
+
+Copyright (c) 2024, Neptune Labs Sp. z o.o.
+
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, softwaredistributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and limitations under the License.
---
+++ utils/migration_tools/model_registry_to_runs/model_to_run.py
@@ -0,0 +1,368 @@
+#
+# Copyright (c) 2024, Neptune Labs Sp. z o.o.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Instructions on how to use this script can be found at
+# https://github.com/neptune-ai/examples/blob/main/utils/migration_tools/from_another_project/README.md
+
+
+# %%
+import contextlib
+import functools
+import logging
+import os
+import shutil
+import time
+import zipfile
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from datetime import datetime
+from glob import glob
+from typing import Literal, Optional, Union
+
+import neptune.metadata_containers
+import pandas as pd
+from neptune import management
+from neptune.exceptions import MetadataInconsistency, MissingFieldException
+from neptune.types import File
+from tqdm.auto import tqdm
+
+# %% Project Name
+print(
+    "Enter the project name (in WORKSPACE_NAME/PROJECT_NAME format). Leave empty to use the `NEPTUNE_PROJECT` environment variable`"
+)
+
+PROJECT = input().strip().lower() or os.getenv("NEPTUNE_PROJECT")
+
+# %% Num Workers
+print("Enter the number of workers to use (int). Leave empty to use all available CPUs")
+
+NUM_WORKERS = int(input().strip() or os.cpu_count())
+
+# %% Setup logger
+
+log_filename = datetime.now().strftime(
+    f"models_migration_{PROJECT.replace('/','_')}_%Y%m%d%H%M%S.log"
+)
+logging.basicConfig(
+    filename=log_filename,
+    filemode="a",
+    format="%(asctime)s %(name)s %(levelname)s %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    level=logging.INFO,
+    force=True,
+)
+
+print(f"Logs available at {log_filename}\n")
+
+# Silencing Neptune messages and urllib connection pool warnings
+logging.getLogger("neptune").setLevel(logging.CRITICAL)
+logging.getLogger("urllib3").setLevel(logging.ERROR)
+
+# %% Create temporary directory to store local metadata
+tmpdirname = "tmp_" + datetime.now().strftime("%Y%m%d%H%M%S")
+os.makedirs(tmpdirname, exist_ok=True)
+logging.info(f"Temporary directory created at {tmpdirname}")
+
+# %% Map namespaces
+
+READ_ONLY_NAMESPACES = {
+    "sys/creation_time",
+    "sys/id",
+    "sys/modification_time",
+    "sys/monitoring_time",
+    "sys/owner",
+    "sys/ping_time",
+    "sys/running_time",
+    "sys/size",
+    "sys/trashed",
+    "sys/stage",
+    "sys/model_id",
+}
+
+MAPPED_NAMESPACES = {
+    namespace: namespace.replace("sys", "old_sys") for namespace in READ_ONLY_NAMESPACES
+}
+
+UNFETCHABLE_NAMESPACES = {
+    "sys/state",
+}
+
+# %% Validate project name
+projects = management.get_project_list()
+
+if PROJECT not in projects:
+    logging.error(f"Project {PROJECT} does not exist. Please check project name")
+    exit()
+else:
+    logging.info(f"Copying Models in {PROJECT} to Runs using {NUM_WORKERS} workers")
+
+# %% Get list of models to be copied
+with neptune.init_project(
+    project=PROJECT,
+    mode="read-only",
+) as neptune_from_project:
+    models = neptune_from_project.fetch_models_table(columns=[]).to_pandas()["sys/id"].values
+
+logging.info(f"{len(models)} models found")
+
+
+# %% UDFs
+
+
+def log_error(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except Exception as e:
+            logging.error(f"Failed to copy {args[4]}/{args[1]} due to exception:\n{e}")
+
+    return wrapper
+
+
+def flatten_namespaces(
+    dictionary: dict, prefix: Optional[list] = None, result: Optional[list] = None
+) -> list:
+    if prefix is None:
+        prefix = []
+    if result is None:
+        result = []
+
+    for k, v in dictionary.items():
+        if isinstance(v, dict):
+            flatten_namespaces(v, prefix + [k], result)
+        elif prefix_str := "/".join(prefix):
+            result.append(f"{prefix_str}/{k}")
+        else:
+            result.append(k)
+    return result
+
+
+@log_error
+def copy_artifacts(object, namespace, run, id):
+    for artifact_location in [
+        artifact.metadata["location"] for artifact in object[namespace].fetch_files_list()
+    ]:
+        run[namespace].track_files(artifact_location)
+
+
+@log_error
+def copy_stringset(object, namespace, run, id):
+    with contextlib.suppress(MissingFieldException, MetadataInconsistency):
+        # Ignore missing `group_tags` field
+        run[namespace].add(object[namespace].fetch())
+
+
+@log_error
+def copy_float_string_series(object, namespace, run, id):
+    for row in object[namespace].fetch_values().itertuples():
+        run[namespace].append(
+            value=row.value,
+            step=row.step,
+            timestamp=time.mktime(pd.to_datetime(row.timestamp).timetuple()),
+        )
+
+
+@log_error
+def copy_file(object, namespace, run, localpath, id):
+    ext = object[namespace].fetch_extension()
+
+    path = os.pathsep.join(namespace.split("/")[:-1])
+    _download_path = os.path.join(localpath, path)
+    os.makedirs(_download_path, exist_ok=True)
+    object[namespace].download(_download_path, progress_bar=False)
+    run[namespace].upload(os.path.join(localpath, namespace) + "." + ext)
+
+
+@log_error
+def copy_fileset(object, namespace, run, localpath, id):
+    _download_path = os.path.join(localpath, namespace)
+    os.makedirs(_download_path, exist_ok=True)
+    object[namespace].download(_download_path, progress_bar=False)
+
+    _zip_path = os.path.join(_download_path, f"{namespace.split('/')[-1]}.zip")
+    with zipfile.ZipFile(_zip_path) as zip_ref:
+        zip_ref.extractall(_download_path)
+    os.remove(_zip_path)
+    run[namespace].upload_files(
+        _download_path,
+    )
+
+
+@log_error
+def copy_fileseries(object, namespace, run, localpath, id):
+    _download_path = os.path.join(localpath, namespace)
+    object[namespace].download(_download_path, progress_bar=False)
+    for file in glob(f"{tmpdirname}{os.pathsep}*"):
+        run[namespace].append(File(file))
+
+
+@log_error
+def copy_atom(object, namespace, run, id):
+    run[namespace] = object[namespace].fetch()
+
+
+def copy_metadata(
+    object: Union[neptune.Model, neptune.ModelVersion],
+    id: str,
+    run: neptune.Run,
+) -> None:
+    namespaces = flatten_namespaces(object.get_structure())
+
+    _local_path = os.path.join(tmpdirname, id)
+
+    for namespace in namespaces:
+        if namespace in UNFETCHABLE_NAMESPACES:
+            continue
+
+        elif namespace in READ_ONLY_NAMESPACES:
+            # Create old_sys namespaces for read-only sys namespaces
+            run[MAPPED_NAMESPACES[namespace]] = object[namespace].fetch()
+
+        elif str(object[namespace]).startswith("<Artifact"):
+            copy_artifacts(object, namespace, run, id)
+
+        elif str(object[namespace]).startswith("<StringSet"):
+            copy_stringset(object, namespace, run, id)
+
+        elif str(object[namespace]).split()[0] in (
+            "<FloatSeries",
+            "<StringSeries",
+        ):
+            copy_float_string_series(object, namespace, run, id)
+
+        elif str(object[namespace]).startswith("<File"):
+            copy_file(object, namespace, run, _local_path, id)
+
+        elif str(object[namespace]).startswith("<FileSet"):
+            copy_fileset(object, namespace, run, _local_path, id)
+
+        elif str(object[namespace]).startswith("<FileSeries"):
+            copy_fileseries(object, namespace, run, _local_path, id)
+
+        else:
+            copy_atom(object, namespace, run, id)
+
+
+def init_target_run(custom_run_id, type: Literal["model", "model_version"]):
+    return neptune.init_run(
+        project=PROJECT,
+        custom_run_id=custom_run_id,  # Assigning model_id/model_version_id as custom_run_id to prevent duplication if the script is rerun
+        tags=[type],
+        capture_hardware_metrics=False,
+        capture_stderr=False,
+        capture_traceback=False,
+        capture_stdout=False,
+        git_ref=False,
+        source_files=[],
+    )
+
+
+def copy_model_version(model_version_id, model_id):
+    with neptune.init_model_version(
+        project=PROJECT,
+        with_id=model_version_id,
+        mode="read-only",
+    ) as model_version:
+        with init_target_run(model_version_id, type="model_version") as model_version_run:
+            model_version_run_id = model_version_run["sys/id"].fetch()
+
+            # Adding model_id as a group tag for easier organization
+            model_version_run["sys/group_tags"].add([model_id])
+
+            copy_metadata(
+                model_version,
+                model_version_id,
+                model_version_run,
+            )
+
+            logging.info(f"Copied {model_version_id} to {model_version_run_id}")
+
+
+def copy_model(model_id):
+    with neptune.init_model(
+        project=PROJECT,
+        with_id=model_id,
+        mode="read-only",
+    ) as model:
+        with init_target_run(model_id, type="model") as model_run:
+            model_run_id = model_run["sys/id"].fetch()
+
+            # Adding model_id as a group tag for easier organization
+            model_run["sys/group_tags"].add([model_id])
+
+            copy_metadata(model, model_id, model_run)
+            logging.info(f"Copied {model_id} to {model_run_id}")
+
+            model_versions = model.fetch_model_versions_table(columns=[]).to_pandas()
+
+            if model_versions.empty:
+                return
+
+            model_versions = (
+                model.fetch_model_versions_table(columns=[]).to_pandas()["sys/id"].values
+            )
+
+            with ThreadPoolExecutor(max_workers=NUM_WORKERS) as executor:
+                future_to_model_version = {
+                    executor.submit(
+                        copy_model_version, model_version_id, model_id
+                    ): model_version_id
+                    for model_version_id in model_versions
+                }
+
+                for future in tqdm(
+                    as_completed(future_to_model_version),
+                    total=len(model_versions),
+                    desc=f"Copying Model Versions for {model_id}",
+                ):
+                    model_version_id = future_to_model_version[future]
+                    try:
+                        future.result()
+                    except Exception as e:
+                        logging.error(f"Failed to copy {model_version_id} due to exception:\n{e}")
+
+
+# %%
+try:
+    with ThreadPoolExecutor(max_workers=NUM_WORKERS) as executor:
+        future_to_model = {executor.submit(copy_model, model_id): model_id for model_id in models}
+
+        for future in tqdm(
+            as_completed(future_to_model),
+            total=len(models),
+            desc="Copying Models",
+            position=0,
+        ):
+            model_id = future_to_model[future]
+            try:
+                future.result()
+            except Exception as e:
+                logging.error(f"Failed to copy {model_id} due to exception:\n{e}")
+
+        logging.info("Export complete!")
+
+except Exception as e:
+    logging.error(f"Error during export: {e}")
+    raise e
+
+finally:
+    logging.info(f"Cleaning up temporary directory {tmpdirname}")
+    try:
+        shutil.rmtree(tmpdirname)
+        logging.info("Done!")
+    except Exception as e:
+        logging.error(f"Failed to remove temporary directory {tmpdirname}\n{e}")
+    finally:
+        logging.shutdown()
