cf43445c402fd144220da6034a59d2d7da114184
Siddhant Sadangi <siddhant.sadangi@gmail.com>
2024-08-07 17:53:23 +0200

    feat: Update e2e example for latest SaaS features

--- how-to-guides/e2e-tracking/scripts/e2e_tracking.py
+++ how-to-guides/e2e-tracking/scripts/e2e_tracking.py
@@ -4,14 +4,15 @@
 #
 # This script shows how you can use Neptune to track a model across all stages of its lifecycle by:
 # * Logging model and run metadata to a central project
-# * Comparing runs to select the best performing model
+# * Grouping models by their stage
+# * Comparing models to select the best performing model
 # * Monitoring a model once in production
 #
 # This script can be used as a template to design an automated end-to-end pipeline that covers the
 # entire lifecycle of a model without needing any manual intervention.
 #
 # This example uses Optuna hyperparameter-optimization to simulate training and evaluating multiple
-# scikit-learn models, and Evidently to monitor models in production.
+# XGBoost models, and Evidently to monitor models in production.
 # However, given Neptune's flexibility and multiple integrations, you can use any library and
 # framework of your choice.
 #
@@ -33,25 +34,23 @@ import matplotlib
 import neptune
 import numpy as np
 import optuna
+import xgboost as xgb
 from evidently.metric_preset import RegressionPreset
 from evidently.metrics import *
 from evidently.report import Report
-from neptune.exceptions import NeptuneModelKeyAlreadyExistsError
 from neptune.integrations.optuna import NeptuneCallback
-from neptune.integrations.sklearn import create_regressor_summary, get_pickled_model
+from neptune.integrations.xgboost import NeptuneCallback as XGBNeptuneCallback
 from neptune.utils import stringify_unsupported
 from sklearn.datasets import fetch_california_housing
-from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
 
 # To prevent `RuntimeError: main thread is not in main loop` error
 matplotlib.use("Agg")
 
 # %% ===== Track model training ===== #
-# 1. Use Optuna to train multiple scikit-learn regression models
-# 2. Leverage Neptune's scikit-learn and Optuna integrations to automatically log metadata and metrics
+# 1. Use Optuna to train multiple XGBoost models
+# 2. Leverage Neptune's XGBoost and Optuna integrations to automatically log metadata and metrics
 #    to Neptune for easy run comparison
-# 3. Use Neptune's model registry to track models
 
 # %%## Initialize the Neptune project
 # To connect to the Neptune app, you need to tell Neptune who you are (`api_token`) and where to send the data (`project`).
@@ -75,85 +74,69 @@ project = neptune.init_project(mode="read-only")
 
 data, target = fetch_california_housing(return_X_y=True)
 X_train, X_test, y_train, y_test = train_test_split(data, target, test_size=0.25)
+dtrain = xgb.DMatrix(X_train, label=y_train)
+dval = xgb.DMatrix(X_test, label=y_test)
 
-
-# %%## Create a new model in the model registry
-# This model will serve as a placeholder for all the model versions created in different Optuna trials
-
-model_key = "RFR"
-
-try:
-    # Create a new model if it does not already exist
-    npt_model = neptune.init_model(key=model_key)
-except NeptuneModelKeyAlreadyExistsError:
-    # Initialize the model if it already exists
-    npt_model = neptune.init_model(with_id=f"{project['sys/id'].fetch()}-{model_key}")
+evals = [(dtrain, "train"), (dval, "valid")]
 
 # %%## Create the Optuna objective function
 # We will create trial level runs and model versions within the objective function to capture
-# trial-level metadata using Neptune's scikit-learn integration.
+# trial-level metadata using Neptune's XGBoost integration.
 
 
 def objective(trial):
+    # Define model parameters
+    model_params = {
+        "max_depth": trial.suggest_int("max_depth", 0, 10),
+        "min_child_weight": trial.suggest_int("min_child_weight", 2, 9),
+        "learning_rate": trial.suggest_float("learning_rate", 0.5, 1.0),
+        "colsample_bytree": trial.suggest_float("colsample_bytree", 0.5, 0.75),
+        "subsample": trial.suggest_float("subsample", 0.4, 1.0),
+        "objective": "reg:squarederror",
+        "eval_metric": ["mae", "rmse"],
+    }
 
-    param = {
-        "n_estimators": trial.suggest_int("n_estimators", 2, 64),
-        "max_depth": trial.suggest_int("max_depth", 2, 5),
-        "min_samples_split": trial.suggest_int("min_samples_split", 3, 10),
+    # Define training parameters
+    train_params = {
+        "num_boost_round": trial.suggest_int("num_boost_round", 10, 50),
     }
 
     # Create a trial-level run
-    run_trial_level = neptune.init_run(
-        capture_hardware_metrics=True,
-        capture_stderr=True,
-        capture_stdout=True,
-        tags=["script", "trial-level"],
-    )
+    run_trial_level = neptune.init_run(tags=["script", "trial-level"])
 
     # Log study name and trial number to trial-level run
     run_trial_level["study-name"] = str(study.study_name)
     run_trial_level["trial-number"] = trial.number
 
-    # Log parameters of a trial-level run
-    run_trial_level["parameters"] = param
+    # Log training parameters of a trial-level run
+    run_trial_level["training/parameters"] = train_params
 
-    # Train the model
-    model = RandomForestRegressor(**param)
-    model.fit(X_train, y_train)
+    # Model parameters are logged automatically by the NeptuneCallback
 
-    # Log model metadata to the trial level run
-    run_trial_level["model_summary"] = create_regressor_summary(
-        model, X_train, X_test, y_train, y_test
+    # Create NeptuneCallback to log trial-level metadata
+    neptune_xgb_callback = XGBNeptuneCallback(run=run_trial_level)
+
+    # Train the model and log trial-level metadata to the trial-level run
+    model = xgb.train(
+        params=model_params,
+        dtrain=dtrain,
+        num_boost_round=train_params["num_boost_round"],
+        evals=evals,
+        verbose_eval=False,
+        callbacks=[
+            neptune_xgb_callback,
+            xgb.callback.LearningRateScheduler(lambda epoch: 0.99**epoch),
+            xgb.callback.EarlyStopping(rounds=10, save_best=True, maximize=False),
+        ],
     )
 
-    # Fetch objective score from the run
-    run_trial_level.wait()
-    score = run_trial_level["model_summary/test/scores/mean_absolute_error"].fetch()
+    # Use group tags to identify the stage of the model
+    run_trial_level["sys/group_tags"].add(["development"])
 
-    # Create a new model version
-    model_version = neptune.init_model_version(model=f"{project['sys/id'].fetch()}-{model_key}")
-
-    # Link model-version to the trial-level run
-    model_version["training/run/id"] = run_trial_level["sys/id"].fetch()
-    model_version["training/run/url"] = run_trial_level.get_url()
-
-    run_trial_level["model_summary/id"] = model_version["sys/id"].fetch()
-    run_trial_level["model_summary/url"] = model_version.get_url()
-
-    # Log score to model version
-    model_version["training/score"] = score
-
-    # Upload model binary to model version
-    model_version["saved_model"].upload(get_pickled_model(model))
-
-    # Update model stage to "staging"
-    model_version.change_stage("staging")
-
-    # Stop model version and trial-level run
-    model_version.stop()
+    # Stop trial-level run
     run_trial_level.stop()
 
-    return score
+    return model.best_score
 
 
 # %%## Create the Optuna study and a Neptune study-level run
@@ -178,66 +161,81 @@ study.optimize(objective, n_trials=5, callbacks=[neptune_optuna_callback])
 
 # %%## Stop the study level run
 run_study_level.stop()
-print("Completed HPO")
+print("Completed Study")
 
 # %% ===== Compare the runs, and choose the best model to move to production ===== #
 
-### Download the model versions table as a pandas dataframe
+### Download the runs table as a pandas dataframe
 
-model_versions_df = npt_model.fetch_model_versions_table(
-    columns=["sys/stage", "training/score"],
-    sort_by="training/score",
+score_namespace = (
+    "training/early_stopping/best_score"  # This is where the best score is logged in Neptune
+)
+
+project = neptune.init_project(mode="read-only")
+
+# %% ### Fetch the champion model. This is the model currently in production
+champion_model_df = project.fetch_runs_table(
+    query='`sys/group_tags`:stringSet CONTAINS "production"',
+    columns=[score_namespace],
+    sort_by=score_namespace,
     ascending=True,
-    progress_bar=False,
+    limit=1,
+).to_pandas()
+
+# %% ### Fetch the challenger model. This is the best model in development
+challenger_model_df = project.fetch_runs_table(
+    query='(`sys/group_tags`:stringSet CONTAINS "development") AND (`sys/tags`:stringSet CONTAINS "trial-level")',
+    columns=[score_namespace],
+    sort_by=score_namespace,
+    ascending=True,
+    limit=1,
 ).to_pandas()
 
 # %%## Get scores and IDs of challenger and champion models
 try:
-    champion_model = model_versions_df[model_versions_df["sys/stage"] == "production"][
-        "sys/id"
-    ].values[0]
-    champion_model_score = model_versions_df[model_versions_df["sys/stage"] == "production"][
-        "training/score"
-    ].values[0]
-    print(f"Champion model ID: {champion_model} and score: {champion_model_score}")
+    champion_model_id = champion_model_df["sys/id"].values[0]
+    champion_model_score = champion_model_df[score_namespace].values[0]
+    print(f"Champion model ID: {champion_model_id} and score: {champion_model_score}")
     NO_CHAMPION = False
-except IndexError:
-    print("No model found in production")
+except KeyError:
+    print("❌ No model found in production")
     NO_CHAMPION = True
 
-staged_models = model_versions_df[model_versions_df["sys/stage"] == "staging"]
-challenger_model_score = min(staged_models["training/score"])
-challenger_model_id = staged_models[staged_models["training/score"] == challenger_model_score][
-    "sys/id"
-].values[0]
+challenger_model_id = challenger_model_df["sys/id"].values[0]
+challenger_model_score = challenger_model_df[score_namespace].values[0]
 
 print(f"Challenger model ID: {challenger_model_id} and score: {challenger_model_score}")
 
 # %%## Promote challenger to champion if score is better
 if NO_CHAMPION:
     print(f"Promoting {challenger_model_id} to Production")
-    with neptune.init_model_version(with_id=challenger_model_id) as challenger_model:
-        challenger_model.change_stage("production")
+    with neptune.init_run(with_id=challenger_model_id) as challenger_model:
+        challenger_model["sys/group_tags"].add("production")
+        challenger_model["sys/group_tags"].remove("development")
 
 elif challenger_model_score < champion_model_score:
     print("Challenger is better than champion")
 
-    print(f"Archiving champion model {champion_model}")
-    with neptune.init_model_version(with_id=champion_model) as champion_model:
-        champion_model.change_stage("archived")
+    print(f"Archiving champion model {champion_model_id}")
+    with neptune.init_run(with_id=champion_model_id) as champion_model:
+        champion_model["sys/group_tags"].remove("production")
+        champion_model["sys/group_tags"].add("archived")
 
     print(f"Promoting {challenger_model_id} to Production")
-    with neptune.init_model_version(with_id=challenger_model_id) as challenger_model:
-        challenger_model.change_stage("production")
+    with neptune.init_run(with_id=challenger_model_id) as challenger_model:
+        challenger_model["sys/group_tags"].add("production")
+        challenger_model["sys/group_tags"].remove("development")
 
 else:
     print("Champion model is better than challenger")
     print(f"Archiving challenger model {challenger_model_id}")
-
+    with neptune.init_run(with_id=challenger_model_id) as challenger_model:
+        challenger_model["sys/group_tags"].add("archived")
+        challenger_model["sys/group_tags"].remove("development")
 
 # %% ===== Monitor model in production ===== #
 # In this section, you will:
-# 1. Download the model binary from the model registry to make predictions in production
+# 1. Download the model binary from the run to make predictions in production
 # 2. Use EvidentlyAI to monitor your model in production.
 #
 # We will use a modified version of a tutorial from the Evidently documentation:
@@ -252,30 +250,36 @@ housing_data = data.frame
 housing_data.rename(columns={"MedHouseVal": "target"}, inplace=True)
 
 reference = housing_data.sample(n=10000, replace=False)
-reference["prediction"] = reference["target"].values + np.random.normal(0, 5, reference.shape[0])
+reference["prediction"] = reference["target"].values + np.random.normal(
+    0, 0.1, reference.shape[0]
+)  # Mocking historical predictions
 
 current = housing_data.sample(n=10000, replace=False)
+dcurrent = xgb.DMatrix(current.drop("target", axis=1), label=current["target"])
 
-# %%## Download saved model from model registry
-model_versions_df = npt_model.fetch_model_versions_table(
-    columns=["sys/stage"], progress_bar=False
-).to_pandas()
+# %%## Download saved model from Neptune
+production_model_id = (
+    project.fetch_runs_table(
+        query='`sys/group_tags`:stringSet CONTAINS "production"',
+        columns=[],
+        sort_by=score_namespace,
+        ascending=True,
+        limit=1,
+    )
+    .to_pandas()["sys/id"]
+    .values[0]
+)
 
-production_model = model_versions_df[model_versions_df["sys/stage"] == "production"][
-    "sys/id"
-].values[0]
-
-npt_model_version = neptune.init_model_version(with_id=production_model)
-npt_model_version["saved_model"].download()
-print("Model binary downloaded to 'saved_model.pkl'")
+production_model = neptune.init_run(with_id=production_model_id)
+production_model["training/pickled_model"].download()
 
 # %%## Make predictions from downloaded model on current test data
 print("Making predictions on current data")
 
-with open("saved_model.pkl", "rb") as f:
+with open("pickled_model.pkl", "rb") as f:
     model = pkl.load(f)
 
-current["prediction"] = model.predict(current.drop(columns=["target"]))
+current["prediction"] = model.predict(dcurrent)
 
 # %%## Generate regression report
 reg_performance_report = Report(metrics=[RegressionPreset()])
@@ -283,22 +287,19 @@ reg_performance_report.run(reference_data=reference, current_data=current)
 
 # %%## Upload report to the model
 reg_performance_report.save_html("report.html")
-npt_model_version["production/report"].upload("report.html")
-print(f"Model report uploaded to {npt_model_version.get_url()}")
+production_model["production/report"].upload("report.html")
+print(f"Model report uploaded to {production_model.get_url()}")
 
 # %%## Upload metrics to the model
-npt_model_version["production/metrics"] = stringify_unsupported(
+production_model["production/metrics"] = stringify_unsupported(
     reg_performance_report.as_dict()["metrics"][0]
 )
-npt_model_version.wait()
+production_model.wait()
 
 # %%## These metrics can then be fetched downstream to trigger a model refresh or retraining, if needed
-retraining_threshold = 0.5  # example
+retraining_threshold = 0.5  # example threshold
 
-if (
-    npt_model_version["production/metrics/result/current/mean_abs_error"].fetch()
-    > retraining_threshold
-):
+if production_model["production/metrics/result/current/rmse"].fetch() > retraining_threshold:
     print("Model degradation detected. Retraining model...")
     ...
 else:
@@ -306,5 +307,4 @@ else:
 
 # %% ===== Stop Neptune objects ===== #
 project.stop()
-npt_model.stop()
-npt_model_version.stop()
+production_model.stop()
