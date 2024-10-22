9d865bdceb0439e811718ba4d5bd42f5568acd8a
Siddhant Sadangi <siddhant.sadangi@gmail.com>
2024-10-22 15:43:25 +0200

    fix: Updated GX example for GX Core 1.x

--- integrations-and-supported-tools/great-expectations/scripts/Neptune_Great_Expectations.py
+++ integrations-and-supported-tools/great-expectations/scripts/Neptune_Great_Expectations.py
@@ -3,6 +3,7 @@ import shutil
 
 import great_expectations as gx
 import neptune
+import pandas as pd
 from great_expectations.data_context import EphemeralDataContext
 from neptune.utils import stringify_unsupported
 
@@ -13,74 +14,106 @@ run = neptune.init_run(
     tags=["script"],  # (optional) replace with your own
 )
 
-# Create a GX Data Context
-context = gx.get_context()
-
-## Upload context configuration to Neptune
-run["gx/context/config"] = context.get_config().to_json_dict()
-
-# Connect to Data
-validator = context.sources.pandas_default.read_csv(
+# Read data
+df = pd.read_csv(
     "https://raw.githubusercontent.com/great-expectations/gx_tutorials/main/data/yellow_tripdata_sample_2019-01.csv"
 )
 
-# Create Expectations
-validator.expect_column_values_to_not_be_null("pickup_datetime")
-validator.expect_column_values_to_be_between("passenger_count", min_value=1, max_value=6)
-validator.save_expectation_suite(
-    discard_failed_expectations=False,
-    discard_catch_exceptions_kwargs=False,
-    discard_include_config_kwargs=False,
-    discard_result_format_kwargs=False,
+# Create a GX Data Context
+context = gx.get_context(mode="file")
+
+# Upload context configuration to Neptune
+run["gx/context/config"] = context.get_config().to_json_dict()
+
+# Connect to Data
+data_source = context.data_sources.add_pandas("pandas")
+data_asset = data_source.add_dataframe_asset(name="pd dataframe asset")
+
+# Create Batch
+batch_definition = data_asset.add_batch_definition_whole_dataframe("batch-def")
+
+batch_definition = (
+    context.data_sources.get("pandas")
+    .get_asset("pd dataframe asset")
+    .get_batch_definition("batch-def")
 )
 
-## Log Expectations to Neptune
-expectation_suite = validator.get_expectation_suite().to_json_dict()
+batch_parameters = {"dataframe": df}
 
-run["gx/meta"] = expectation_suite["meta"]
+batch = batch_definition.get_batch(batch_parameters=batch_parameters)
 
-run["gx/expectations/expectations_suite_name"] = expectation_suite["expectation_suite_name"]
+# Create Expectation Suite
+suite = gx.ExpectationSuite(name="expectation_suite")
+suite = context.suites.add(suite)
 
-for idx, expectation in enumerate(expectation_suite["expectations"]):
+suite.add_expectation(
+    gx.expectations.ExpectColumnValuesToBeBetween(
+        column="passenger_count", min_value=1, max_value=6
+    )
+)
+suite.add_expectation(
+    gx.expectations.ExpectColumnValuesToBeBetween(column="fare_amount", min_value=0)
+)
+suite.add_expectation(gx.expectations.ExpectColumnValuesToNotBeNull(column="pickup_datetime"))
+
+# Log Expectations to Neptune
+run["gx/meta"] = suite.meta
+
+run["gx/expectations/expectations_suite_name"] = suite.name
+
+for idx, expectation in enumerate(suite.to_json_dict()["expectations"]):
     run["gx/expectations"][idx] = expectation
 
-# Create Checkpoint
-checkpoint = context.add_or_update_checkpoint(
-    name="my_quickstart_checkpoint",
-    validator=validator,
+# Create a Validation Definition
+definition_name = "validation_definition"
+validation_definition = gx.ValidationDefinition(
+    data=batch_definition, suite=suite, name=definition_name
 )
 
-## Log Checkpoint configuration to Neptune
-run["gx/checkpoint/config"] = stringify_unsupported(checkpoint.config.to_json_dict())
+# Create Checkpoint
+checkpoint_name = "my_checkpoint"
+
+actions = [
+    gx.checkpoint.UpdateDataDocsAction(name="update_all_data_docs"),
+]
+
+checkpoint = gx.Checkpoint(
+    name=checkpoint_name,
+    validation_definitions=[validation_definition],
+    actions=actions,
+    result_format={"result_format": "COMPLETE"},
+)
+
+context.validation_definitions.add(validation_definition)
+
+context.checkpoints.add(checkpoint)
 
 # Run Validations
-checkpoint_result = checkpoint.run()
+results = checkpoint.run(batch_parameters=batch_parameters)
 
-## Log Validation results to Neptune
-results_dict = checkpoint_result.list_validation_results()[0].to_json_dict()
+# Log Validation results to Neptune
+run["gx/validations/success"] = results.describe_dict()["success"]
 
-run["gx/validations/json"] = results_dict
+run["gx/validations/json"] = results.describe_dict()["validation_results"][0]
 
-for idx, result in enumerate(results_dict["results"]):
-    run["gx/validations/json/results"][idx] = result
+for idx, result in enumerate(results.describe_dict()["validation_results"][0]["expectations"]):
+    run["gx/validations/json/results"][idx] = stringify_unsupported(result)
 
 # Upload HTML reports to Neptune
-## Get the `local_site_path` of the Data Context
-
 if isinstance(context, EphemeralDataContext):
     context = context.convert_to_file_context()
 
 local_site_path = os.path.dirname(context.build_data_docs()["local_site"])[7:]
 
-## Log Expectations reports to Neptune
+# Upload Expectations Reports to Neptune
 run["gx/expectations/reports"].upload_files(os.path.join(local_site_path, "expectations"))
 
-## Log Validations reports to Neptune
+# Upload Validations Reports to Neptune
 run["gx/validations/reports"].upload_files(os.path.join(local_site_path, "validations"))
 
 # Cleanup
-## Stop Neptune run
+# Stop Neptune run
 run.stop()
 
-## (Optional) Delete the FileSystem Data Context
+# Delete FileSystem Data Context
 shutil.rmtree("gx")
